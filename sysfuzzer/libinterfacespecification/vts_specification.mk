e5a263643d8551298b5464a25c4a2f1ba68b183e
Keun Soo Yim <yim@google.com>
2016-03-11 13:22:52 -0800

    gps and camera hal's basic spec files
    also includes:
    - an update of the light hal's spec using the latest proto
      format.
    - build file to copy all registered spec files
    
    Change-Id: I9b007ee40bcade88e8d38f704c879bf3398d3ad3

---
+++ sysfuzzer/libinterfacespecification/vts_specification.mk
@@ -0,0 +1,6 @@
+# mk file to copy VTS specs as part of packaging
+#
+PRODUCT_COPY_FILES += \
+    test/vts/sysfuzzer/libinterfacespecification/specification/CameraHalV1.vts:system/etc/CameraHalV1.vts \
+    test/vts/sysfuzzer/libinterfacespecification/specification/GpsHalV1.vts:system/etc/GpsHalV1.vts \
+    test/vts/sysfuzzer/libinterfacespecification/specification/LightHalV1.vts:system/etc/LightHalV1.vts
