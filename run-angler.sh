39bc0b92c6bdf6ba0c18df18c5d19c424b9cb9f5
Keun Soo Yim <yim@google.com>
2016-07-06 18:27:15 -0700

    mirror baseline for shared lib components and a sample fuzz test case for libm of bionic
    Change-Id: Ic53184370bb36ef832acc65ab2b09afa538351a3

--- run-angler.sh
+++ run-angler.sh
@@ -3,3 +3,4 @@
 PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleLightFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleLightFuzzTest.config
 # PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleCameraTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleCameraTest.config
 # PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleShellTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleShellTest.config
+# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.fuzz_test.lib_bionic.LibBionicLibmFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/fuzz_test/lib_bionic/LibBionicLibmFuzzTest.config
