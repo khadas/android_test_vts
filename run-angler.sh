53413ff7aca5965817f6e0c96a377e5af7767f79
Keun Soo Yim <yim@google.com>
2016-07-11 21:15:40 -0700

    baseline nfc hidl hal testcase
    Change-Id: I486a5a7a687aeea2dd9dd671054dfaef08817a8a

--- run-angler.sh
+++ run-angler.sh
@@ -2,5 +2,6 @@
 
 PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleLightFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleLightFuzzTest.config
 # PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleCameraTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleCameraTest.config
+# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.nfc.NfcHidlBasicTest $ANDROID_BUILD_TOP/test/vts/testcases/host/nfc/NfcHidlBasicTest.config 
 # PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleShellTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleShellTest.config
 # PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.fuzz_test.lib_bionic.LibBionicLibmFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/fuzz_test/lib_bionic/LibBionicLibmFuzzTest.config
---
+++ testcases/host/nfc/NfcHidlBasicTest.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.4
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging
+
+from vts.runners.host import base_test
+from vts.runners.host import test_runner
+from vts.utils.python.controllers import android_device
+
+
+class NfcHidlBasicTest(base_test.BaseTestClass):
+    """A simple testcase for the NFC HIDL HAL."""
+
+    def setUpClass(self):
+        self.dut = self.registerController(android_device)[0]
+        self.dut.hal.InitHidlHal(target_type="nfc",
+                                 target_basepaths=["/data/local/tmp"],
+                                 target_version=1.0,
+                                 bits=64)
+
+    def testBase(self):
+        """A simple testcase which just calls functions."""
+        # TODO: extend to make realistic testcases
+        self.dut.hal.nfc.core_initialized(0)
+        result = self.dut.hal.nfc.close()
+        logging.info("result: %d", result)
+
+
+if __name__ == "__main__":
+    test_runner.main()
