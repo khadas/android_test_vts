038d0ad22853f9c0a97f256f800ec0e673a10e5f
Keun Soo Yim <yim@google.com>
2016-07-12 17:01:44 -0700

    reorg vts host-driven testcases dir
    samples to each hal dir so that component owners can start to use
    as a base.
    
    Change-Id: I2dea3f9111920bf77d791b8a601bc6c198791b09

--- run-angler.sh
+++ run-angler.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleLightFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleLightFuzzTest.config
-# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleCameraTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleCameraTest.config
-# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.nfc.NfcHidlBasicTest $ANDROID_BUILD_TOP/test/vts/testcases/host/nfc/NfcHidlBasicTest.config 
-# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.sample.SampleShellTest $ANDROID_BUILD_TOP/test/vts/testcases/host/sample/SampleShellTest.config
+PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.light.conventional.SampleLightTest $ANDROID_BUILD_TOP/test/vts/testcases/host/light/conventional/SampleLightTest.config
+# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.light.conventional_fuzz.SampleLightFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/host/light/conventional_fuzz/SampleLightFuzzTest.config
+# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.camera.conventional.SampleCameraTest $ANDROID_BUILD_TOP/test/vts/testcases/host/camera/conventional/SampleCameraTest.config
+# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.nfc.hidl.NfcHidlBasicTest $ANDROID_BUILD_TOP/test/vts/testcases/host/nfc/hidl/NfcHidlBasicTest.config
+# PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.host.shell.SampleShellTest $ANDROID_BUILD_TOP/test/vts/testcases/host/shell/SampleShellTest.config
 # PYTHONPATH=$PYTHONPATH:.. python -m vts.testcases.fuzz_test.lib_bionic.LibBionicLibmFuzzTest $ANDROID_BUILD_TOP/test/vts/testcases/fuzz_test/lib_bionic/LibBionicLibmFuzzTest.config
