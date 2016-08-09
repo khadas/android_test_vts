e8c0c39f291fe09c7b7810330d40dad84964ccbf
Ryan Campbell <ryanjcampbell@google.com>
2016-08-09 16:54:09 -0700

    Implement support for multiple files.
    Allow for multiple file support by breaking up coverage report
    generation into three functions. GenerateCoverageVector() uses
    the GCNO/GCDA files to produce a list of integer values
    representing execution counts at each line of code in the source.
    GenerateCoverageHTML() uses the src code and a list of counts to
    produce an HTML coverage report. GenerateCoverageReport()
    combines the two end-to-end.
    
    Change-Id: Iea0621cef01cec3ce442446600745ce90a906755

--- utils/python/coverage/coverage_report.py
+++ utils/python/coverage/coverage_report.py
@@ -19,7 +19,9 @@
 The GenerateCoverageReport() function returns HTML to display the coverage
 at each line of code in a provided source file. Coverage information is
 parsed from .gcno and .gcda file contents and combined with the source file
-to reconstruct a coverage report.
+to reconstruct a coverage report. GenerateLineCoverageVector() is a helper
+function that produces a vector of line counts and GenerateCoverageHTML()
+uses the vector and source to produce the HTML coverage report.
 """
 
 import cgi
@@ -28,6 +30,108 @@ import logging
 import os
 from vts.utils.python.coverage import gcda_parser
 from vts.utils.python.coverage import gcno_parser
+
+TABLE_STYLE = (
+    "border=0 style=\"width: 100%; word-spacing: 5px;"
+    "font-family: monospace; white-space:PRE; border-collapse: collapse\"")
+
+COUNT_STYLE = (
+    "style=\"white-space:nowrap; text-align:right; border-right: 1px "
+    "solid black; padding-right: 5px; font-style: bold\"")
+
+LINE_NO_STYLE = (
+    "style=\"padding-left: 35px; white-space:nowrap; padding-right:5px; "
+    "border-right: 1px dotted gray\"")
+
+SRC_LINE_STYLE = "style=\"padding-left: 10px;width:99%\""
+
+UNCOVERED_STYLE = "bgcolor=\"LightPink\""
+
+COVERED_STYLE = "bgcolor=\"LightGreen\""
+
+
+def GenerateLineCoverageVector(src_file_name, src_file_length,
+                               gcno_file_content, gcda_file_content):
+    """Returns a list of invocation counts for each line in the file.
+
+    Parses the GCNO and GCDA file specified to calculate the number of times
+    each line in the source file specified by src_file_name was executed.
+
+    Args:
+        src_file_name: string, the source file name.
+        src_file_length: int, the number of lines in the src file.
+        gcno_file_content: string, the raw gcno binary file content.
+        gcda_file_content: string, the raw gcda binary file content.
+
+    Returns:
+        A list of integers (or None) representing the number of times the
+        i-th line was executed. None indicates a line that is not executable.
+    """
+    if gcno_file_content:
+        logging.info("GenerateLineCoverageVector: gcno_file_content %d bytes",
+                     len(gcno_file_content))
+    if gcda_file_content:
+        logging.info("GenerateLineCoverageVector: gcda_file_content %d bytes",
+                     len(gcda_file_content))
+    gcno_stream = io.BytesIO(gcno_file_content)
+    file_summary = gcno_parser.GCNOParser(gcno_stream).Parse()
+    gcda_stream = io.BytesIO(gcda_file_content)
+    gcda_parser.GCDAParser(gcda_stream, file_summary).Parse()
+    src_lines_counts = [None] * src_file_length
+    logging.info("GenerateLineCoverageVector: src file lines %d",
+                 src_file_length)
+    for ident in file_summary.functions:
+        func = file_summary.functions[ident]
+        if not src_file_name.endswith(os.path.basename(func.src_file_name)):
+            logging.warn("GenerateLineCoverageVector: %s file is skipped",
+                         func.src_file_name)
+            continue
+        for block in func.blocks:
+            for line in block.lines:
+                logging.info("GenerateLineCoverageVector: covered line %s",
+                             line)
+                if line >= 0 and line < len(src_lines_counts):
+                    if src_lines_counts[line - 1] is None:
+                        src_lines_counts[line - 1] = 0
+                    src_lines_counts[line - 1] += block.count
+                else:
+                    logging.error(
+                        "GenerateLineCoverageVector: line mismatch %s", line)
+    return src_lines_counts
+
+
+def GenerateCoverageHTML(src_file_content, src_lines_counts):
+    """Generates an HTML coverage report given the source and the counts.
+
+    Outputs an HTML file containing coverage information for the provided
+    source file. Creates a table with a row for each line of code and a
+    cell for the coverage count, line number, and source code.
+
+    Args:
+        src_file_content: string, the C/C++ source file content.
+        src_lines_counts: A list of integers (or None) representing the
+                          number of times the i-th line was executed. None
+                          indicates the line is not executable.
+
+    Returns:
+       the coverage HTML produced for the src_file_content.
+    """
+    html = "<div><table %s>\n" % TABLE_STYLE
+    src_lines = src_file_content.split('\n')
+    for line_no in range(len(src_lines)):
+        if src_lines_counts[line_no] is None:  #  Not executable
+            html += "<tr>\n<td %s>--</td>\n" % COUNT_STYLE
+        elif not src_lines_counts[line_no]:  #  Uncovered line
+            html += "<tr %s>\n<td %s>%i</td>\n" % (
+                UNCOVERED_STYLE, COUNT_STYLE, src_lines_counts[line_no])
+        else:  #  covered line
+            html += "<tr %s>\n<td %s>%i</td>\n" % (COVERED_STYLE, COUNT_STYLE,
+                                                   src_lines_counts[line_no])
+        html += "<td %s>%i</td>\n" % (LINE_NO_STYLE, line_no + 1)
+        html += "<td %s>%s</td>\n</tr>\n" % (SRC_LINE_STYLE,
+                                             cgi.escape(src_lines[line_no]))
+    html += "</table></div>"
+    return html
 
 
 def GenerateCoverageReport(src_file_name, src_file_content, gcno_file_content,
@@ -46,61 +150,7 @@ def GenerateCoverageReport(src_file_name, src_file_content, gcno_file_content,
     Returns:
         the coverage HTML produced for 'src_file_name'.
     """
-    logging.info("GenerateCoverageReport: src_file_content %s",
-                 src_file_content)
-    if gcno_file_content:
-        logging.info("GenerateCoverageReport: gcno_file_content %d bytes",
-                     len(gcno_file_content))
-    if gcda_file_content:
-        logging.info("GenerateCoverageReport: gcda_file_content %d bytes",
-                     len(gcda_file_content))
-    gcno_stream = io.BytesIO(gcno_file_content)
-    file_summary = gcno_parser.GCNOParser(gcno_stream).Parse()
-    gcda_stream = io.BytesIO(gcda_file_content)
-    gcda_parser.GCDAParser(gcda_stream, file_summary).Parse()
     src_lines = src_file_content.split('\n')
-    src_lines_counts = [None] * len(src_lines)
-    logging.info("GenerateCoverageReport: src file lines %d", len(src_lines))
-    for ident in file_summary.functions:
-        func = file_summary.functions[ident]
-        if not src_file_name.endswith(os.path.basename(func.src_file_name)):
-            logging.warn("GenerateCoverageReport: %s file is skipped", func.src_file_name)
-            continue
-        for block in func.blocks:
-            for line in block.lines:
-                logging.info("GenerateCoverageReport: covered line %s", line)
-                if line >= 0 and line < len(src_lines_counts):
-                    if src_lines_counts[line - 1] == None:
-                        src_lines_counts[line - 1] = 0
-                    src_lines_counts[line - 1] += block.count
-                else:
-                    logging.error("GenerateCoverageReport: line mismatch %s",
-                                  line)
-
-    html = (
-        "<div><table border=0 style=\"width: 100%; word-spacing: 5px;"
-        "font-family: monospace; white-space:PRE; border-collapse: collapse\">\n"
-    )
-    count_style = (
-        "style=\"white-space:nowrap; text-align:right; border-right: 1px "
-        "solid black; padding-right: 5px; font-style: bold\"")
-    line_no_style = (
-        "style=\"padding-left: 35px; white-space:nowrap; padding-right:5px; "
-        "border-right: 1px dotted gray\"")
-    src_line_style = "style=\"padding-left: 10px;width:99%\""
-
-    for line_no in range(len(src_lines)):
-        if src_lines_counts[
-                line_no] == None:  #  Not compiled to a machine instruction
-            html += "<tr>\n<td %s>--</td>\n" % count_style
-        elif not src_lines_counts[line_no]:  #  Uncovered line
-            html += "<tr bgcolor=\"LightPink\">\n<td %s>%i</td>\n" % (
-                count_style, src_lines_counts[line_no])
-        else:  #  covered line
-            html += "<tr bgcolor=\"LightGreen\">\n<td %s>%i</td>\n" % (
-                count_style, src_lines_counts[line_no])
-        html += "<td %s>%i</td>\n" % (line_no_style, line_no)
-        html += "<td %s>%s</td>\n</tr>\n" % (src_line_style,
-                                         cgi.escape(src_lines[line_no]))
-    html += "</table></div>"
-    return html
+    src_lines_counts = GenerateLineCoverageVector(
+        src_file_name, len(src_lines), gcno_file_content, gcda_file_content)
+    return GenerateCoverageHTML(src_file_content, src_lines_counts)
--- utils/python/coverage/coverage_report_test.py
+++ utils/python/coverage/coverage_report_test.py
@@ -22,30 +22,87 @@ from vts.utils.python.coverage import coverage_report
 
 
 class CoverageReportTest(unittest.TestCase):
-    """End-to-end tests for CoverageReport of vts.utils.python.coverage.
+    """Unit tests for CoverageReport of vts.utils.python.coverage.
     """
 
-    def testSampleFile(self):
-        """Verifies that sample file coverage is generated correctly.
-        """
+    @classmethod
+    def setUpClass(cls):
         root_dir = os.path.join(
             os.getenv('ANDROID_BUILD_TOP'),
             'test/vts/utils/python/coverage/testdata/')
         src_path = os.path.join(root_dir, 'sample.c')
         with open(src_path, 'r') as file:
-            src_content = file.read()
+            cls.src_content = file.read()
         gcno_path = os.path.join(root_dir, 'sample.gcno')
         with open(gcno_path, 'rb') as file:
-            gcno_content = file.read()
+            cls.gcno_content = file.read()
         gcda_path = os.path.join(root_dir, 'sample.gcda')
         with open(gcda_path, 'rb') as file:
-            gcda_content = file.read()
+            cls.gcda_content = file.read()
         html_path = os.path.join(root_dir, 'sample_coverage.html')
         with open(html_path, 'r') as file:
-            html_content = file.read()
-        html_generated = coverage_report.GenerateCoverageReport('sample.c', src_content,
-            gcno_content, gcda_content)
-        self.assertEqual(html_content, html_generated)
+            cls.html_content = file.read()
+
+    def testGenerateCoverageHTML(self):
+        """Tests that coverage HTML is correctly generated.
+
+        Runs GenerateCoverageHTML on mocked source file and count
+        files.
+        """
+        src = 'test\nfile\ncontents'
+        src_lines = src.split('\n')
+        src_lines_counts = [10, None, 0]
+        expected = []
+        expected.append('<div><table %s>' % coverage_report.TABLE_STYLE)
+        expected.append('<tr %s>' % coverage_report.COVERED_STYLE)
+        expected.append('<td %s>%i</td>' %
+                     (coverage_report.COUNT_STYLE, src_lines_counts[0]))
+        expected.append('<td %s>%i</td>' % (coverage_report.LINE_NO_STYLE, 1))
+        expected.append('<td %s>%s</td>' %
+                        (coverage_report.SRC_LINE_STYLE, src_lines[0]))
+        expected.append('</tr>')
+
+        expected.append('<tr>')
+        expected.append('<td %s>%s</td>' % (coverage_report.COUNT_STYLE, '--'))
+        expected.append('<td %s>%i</td>' % (coverage_report.LINE_NO_STYLE, 2))
+        expected.append('<td %s>%s</td>' %
+                        (coverage_report.SRC_LINE_STYLE, src_lines[1]))
+        expected.append('</tr>')
+
+        expected.append('<tr %s>' % coverage_report.UNCOVERED_STYLE)
+        expected.append('<td %s>%i</td>' %
+                        (coverage_report.COUNT_STYLE, src_lines_counts[2]))
+        expected.append('<td %s>%i</td>' % (coverage_report.LINE_NO_STYLE, 3))
+        expected.append('<td %s>%s</td>' %
+                        (coverage_report.SRC_LINE_STYLE, src_lines[2]))
+        expected.append('</tr>')
+        expected.append('</table></div>')
+
+        generated = coverage_report.GenerateCoverageHTML(src, src_lines_counts)
+        self.assertEqual(generated, '\n'.join(expected))
+
+    def testGenerateLineCoverageVector(self):
+        """Tests that coverage vector is correctly generated.
+
+        Runs GenerateLineCoverageVector on sample file and checks
+        result.
+        """
+        src_lines = self.src_content.split('\n')
+        src_lines_counts = coverage_report.GenerateLineCoverageVector(
+            'sample.c', len(src_lines), self.gcno_content, self.gcda_content)
+        expected = [None, None, None, None, 2, None, None, None, None, None, 2,
+                    2, 2, None, 2, None, 2, 0, None, 2, None, None, 2, 2, 502,
+                    500, None, None, 2, None, 2, None, None, None, 2, None,
+                    None, None, None, 2, 2, 2, None]
+        self.assertEqual(src_lines_counts, expected)
+
+    def testSampleFileReportEndToEnd(self):
+        """Verifies that sample file coverage is generated correctly (end-to-end)
+        """
+
+        html_generated = coverage_report.GenerateCoverageReport(
+            'sample.c', self.src_content, self.gcno_content, self.gcda_content)
+        self.assertEqual(self.html_content, html_generated)
 
 
 if __name__ == "__main__":
