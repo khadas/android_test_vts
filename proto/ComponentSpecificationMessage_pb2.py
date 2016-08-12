391057e848865531826ddc606dedd804329e7a4d
Keun Soo Yim <yim@google.com>
2016-08-12 13:42:40 -0700

    use singular as the naming style of repeated fields to be consistent
    Change-Id: Id34066af78d12f3444281ea9cb7c13444fb7bcc6

--- proto/ComponentSpecificationMessage_pb2.py
+++ proto/ComponentSpecificationMessage_pb2.py
@@ -14,7 +14,7 @@ import InterfaceSpecificationMessage_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='ComponentSpecificationMessage.proto',
   package='android.vts',
-  serialized_pb='\n#ComponentSpecificationMessage.proto\x12\x0b\x61ndroid.vts\x1a#InterfaceSpecificationMessage.proto\"\x9d\x03\n\x1d\x43omponentSpecificationMessage\x12\x34\n\x0f\x63omponent_class\x18\x01 \x01(\x0e\x32\x1b.android.vts.ComponentClass\x12\x32\n\x0e\x63omponent_type\x18\x02 \x01(\x0e\x32\x1a.android.vts.ComponentType\x12!\n\x16\x63omponent_type_version\x18\x03 \x01(\x02:\x01\x31\x12\x16\n\x0e\x63omponent_name\x18\x04 \x01(\x0c\x12\x0f\n\x07package\x18\x0b \x01(\x0c\x12\x0e\n\x06import\x18\x0c \x03(\x0c\x12%\n\x1coriginal_data_structure_name\x18\xe9\x07 \x01(\x0c\x12\x0f\n\x06header\x18\xea\x07 \x03(\x0c\x12>\n\tinterface\x18\xd1\x0f \x01(\x0b\x32*.android.vts.InterfaceSpecificationMessage\x12>\n\nattributes\x18\xb5\x10 \x03(\x0b\x32).android.vts.VariableSpecificationMessage')
+  serialized_pb='\n#ComponentSpecificationMessage.proto\x12\x0b\x61ndroid.vts\x1a#InterfaceSpecificationMessage.proto\"\x9c\x03\n\x1d\x43omponentSpecificationMessage\x12\x34\n\x0f\x63omponent_class\x18\x01 \x01(\x0e\x32\x1b.android.vts.ComponentClass\x12\x32\n\x0e\x63omponent_type\x18\x02 \x01(\x0e\x32\x1a.android.vts.ComponentType\x12!\n\x16\x63omponent_type_version\x18\x03 \x01(\x02:\x01\x31\x12\x16\n\x0e\x63omponent_name\x18\x04 \x01(\x0c\x12\x0f\n\x07package\x18\x0b \x01(\x0c\x12\x0e\n\x06import\x18\x0c \x03(\x0c\x12%\n\x1coriginal_data_structure_name\x18\xe9\x07 \x01(\x0c\x12\x0f\n\x06header\x18\xea\x07 \x03(\x0c\x12>\n\tinterface\x18\xd1\x0f \x01(\x0b\x32*.android.vts.InterfaceSpecificationMessage\x12=\n\tattribute\x18\xb5\x10 \x03(\x0b\x32).android.vts.VariableSpecificationMessage')
 
 
 
@@ -90,7 +90,7 @@ _COMPONENTSPECIFICATIONMESSAGE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='attributes', full_name='android.vts.ComponentSpecificationMessage.attributes', index=9,
+      name='attribute', full_name='android.vts.ComponentSpecificationMessage.attribute', index=9,
       number=2101, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -106,13 +106,13 @@ _COMPONENTSPECIFICATIONMESSAGE = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=90,
-  serialized_end=503,
+  serialized_end=502,
 )
 
 _COMPONENTSPECIFICATIONMESSAGE.fields_by_name['component_class'].enum_type = InterfaceSpecificationMessage_pb2._COMPONENTCLASS
 _COMPONENTSPECIFICATIONMESSAGE.fields_by_name['component_type'].enum_type = InterfaceSpecificationMessage_pb2._COMPONENTTYPE
 _COMPONENTSPECIFICATIONMESSAGE.fields_by_name['interface'].message_type = InterfaceSpecificationMessage_pb2._INTERFACESPECIFICATIONMESSAGE
-_COMPONENTSPECIFICATIONMESSAGE.fields_by_name['attributes'].message_type = InterfaceSpecificationMessage_pb2._VARIABLESPECIFICATIONMESSAGE
+_COMPONENTSPECIFICATIONMESSAGE.fields_by_name['attribute'].message_type = InterfaceSpecificationMessage_pb2._VARIABLESPECIFICATIONMESSAGE
 DESCRIPTOR.message_types_by_name['ComponentSpecificationMessage'] = _COMPONENTSPECIFICATIONMESSAGE
 
 class ComponentSpecificationMessage(_message.Message):
