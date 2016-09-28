285f6b140d14001ba0ac889dd9bcdc5a24d87af2
Zhuoyao Zhang <zhuoyao@google.com>
2016-09-28 11:01:13 -0700

    Add a python script to test the output of vtsc.
    * Runs vtsc with specified mode on a bunch of files and compares the
      output results with canonical ones.
    * script usage: python test_vtsc.py path_to_vtsc canonical_dir output_dir
    * run test: make vtsc_test
    
    Test: make vtsc_test
    Change-Id: I35522c937069d19e0a9bc8a9f048f4c9613baba4

---
+++ sysfuzzer/vtscompiler/test/data/nfc.profiler.cpp
@@ -0,0 +1,208 @@
+#include "test/vts/specification/hal_hidl/Nfc/Nfc.vts.h"
+#include "test/vts/specification/hal_hidl/Nfc/types.vts.h"
+
+using namespace android::hardware;
+using namespace android::hardware::nfc::V1_0;
+
+namespace android {
+namespace vts {
+
+
+void HIDL_INSTRUMENTATION_FUNCTION(
+        InstrumentationEvent event,
+        const char* package,
+        const char* version,
+        const char* interface,
+        const char* method,
+        std::vector<void *> *args) {
+    if (strcmp(package, "android.hardware.nfc") != 0) {
+        LOG(WARNING) << "incorrect package.";
+        return;
+    }
+    if (strcmp(version, "1.0") != 0) {
+        LOG(WARNING) << "incorrect version.";
+        return;
+    }
+    if (strcmp(interface, "INfc") != 0) {
+        LOG(WARNING) << "incorrect interface.";
+        return;
+    }
+    if (strcmp(method, "open") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                auto *arg_0 = msg.add_arg();
+                INfcClientCallback *arg_val_0 = reinterpret_cast<INfcClientCallback*> ((*args)[0]);
+                arg_0->set_type(TYPE_HIDL_CALLBACK);
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+    if (strcmp(method, "write") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                auto *arg_0 = msg.add_arg();
+                nfc_data_t *arg_val_0 = reinterpret_cast<nfc_data_t*> ((*args)[0]);
+                arg_0->set_type(TYPE_STRUCT);
+                profile__nfc_data_t(arg_0, (*arg_val_0));
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+    if (strcmp(method, "core_initialized") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                auto *arg_0 = msg.add_arg();
+                hidl_vec<uint8_t> *arg_val_0 = reinterpret_cast<hidl_vec<uint8_t>*> ((*args)[0]);
+                for (int i = 0; i < (int)(*arg_val_0).size(); i++) {
+                    auto *arg_0_vector_i = arg_0->add_vector_value();
+                    arg_0_vector_i->set_type(TYPE_SCALAR);
+                    arg_0_vector_i->mutable_scalar_value()->set_uint8_t((*arg_val_0)[i]);
+                }
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+    if (strcmp(method, "pre_discover") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+    if (strcmp(method, "close") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+    if (strcmp(method, "control_granted") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+    if (strcmp(method, "power_cycle") == 0) {
+        FunctionSpecificationMessage msg;
+        switch (event) {
+            case SERVER_API_ENTRY:
+            {
+                break;
+            }
+            case SERVER_API_EXIT:
+            {
+                auto *result_0 = msg.add_return_type_hidl();
+                int32_t *result_val_0 = reinterpret_cast<int32_t*> ((*args)[0]);
+                result_0->set_type(TYPE_SCALAR);
+                result_0->mutable_scalar_value()->set_int32_t((*result_val_0));
+                break;
+            }
+            default:
+            {
+                LOG(WARNING) << "not supported. ";
+                break;
+            }
+        }
+        VtsProfilingInterface::getInstance().AddTraceEvent(msg);
+    }
+}
+
+}  // namespace vts
+}  // namespace android
