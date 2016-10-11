cb0611ef32561981a35556df5380e910700618fa
Ryan Campbell <ryanjcampbell@google.com>
2016-10-10 18:37:39 -0700

    Implement user preferences in VTS dashboard.
    Implement user preferences for the main dashboard. Filter which
    tests show up on dashboard home. Allow for users to subscribe to
    email notifications individually via the web interface. This
    functionality is added in a new preferences page that can handle
    get and put requests.
    
    Note, changes to the alert service will be made in a separate
    CL.
    
    Test: tested on local gcloud
    Change-Id: I6e1a00abcd652a4e2ea3db5835b77cea6999593e

--- web/dashboard/appengine/servlet/src/main/java/com/google/android/vts/servlet/DashboardMainServlet.java
+++ web/dashboard/appengine/servlet/src/main/java/com/google/android/vts/servlet/DashboardMainServlet.java
@@ -19,13 +19,23 @@ package com.google.android.vts.servlet;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -40,6 +50,21 @@ import javax.servlet.http.HttpServletResponse;
 public class DashboardMainServlet extends HttpServlet {
 
     private static final String DASHBOARD_MAIN_JSP = "/dashboard_main.jsp";
+    private static final String DASHBOARD_ALL_LINK = "/?showAll=true";
+    private static final String DASHBOARD_FAVORITES_LINK = "/";
+    private static final byte[] EMAIL_FAMILY = Bytes.toBytes("email_to_test");
+    private static final byte[] TEST_FAMILY = Bytes.toBytes("test_to_email");
+    private static final String STATUS_TABLE = "vts_status_table";
+    private static final String TABLE_PREFIX = "result_";
+    private static final String ALL_HEADER = "All Tests";
+    private static final String FAVORITES_HEADER = "Favorites";
+    private static final String NO_FAVORITES_ERROR = "No subscribed tests. Click the edit button to add to favorites.";
+    private static final String NO_TESTS_ERROR = "No test results available.";
+    private static final String FAVORITES_BUTTON = "Show Favorites";
+    private static final String ALL_BUTTON = "Show All";
+    private static final String UP_ARROW = "keyboard_arrow_up";
+    private static final String DOWN_ARROW = "keyboard_arrow_down";
+
     private static final Logger logger = LoggerFactory.getLogger(DashboardMainServlet.class);
 
 
@@ -48,31 +73,85 @@ public class DashboardMainServlet extends HttpServlet {
         UserService userService = UserServiceFactory.getUserService();
         User currentUser = userService.getCurrentUser();
         RequestDispatcher dispatcher = null;
+        String loginURI = userService.createLoginURL(request.getRequestURI());
+        String logoutURI = userService.createLogoutURL(loginURI);
 
-        if (currentUser != null) {
-            response.setContentType("text/plain");
-            HTableDescriptor[] tables = BigtableHelper.getTables();
-            List<String> testList = new ArrayList<>();
+        Table table = BigtableHelper.getTable(TableName.valueOf(STATUS_TABLE));
+        List<String> displayedTests = new ArrayList<>();
 
-            // filter tables not starting with 'result'
-            for (HTableDescriptor table : tables) {
-                String tableName = table.getNameAsString();
-                if (tableName.startsWith("result_")) {
-                    testList.add(tableName.substring(7));  // cut the prefix
+        HTableDescriptor[] tables = BigtableHelper.getTables();
+        Set<String> allTables = new HashSet<>();
+        boolean showAll = request.getParameter("showAll") != null;
+        String header;
+        String buttonLabel;
+        String buttonIcon;
+        String buttonLink;
+        String error = null;
+
+        for (HTableDescriptor descriptor : tables) {
+            String tableName = descriptor.getNameAsString();
+            if (tableName.startsWith(TABLE_PREFIX)) {
+                allTables.add(tableName);
+            }
+        }
+
+        if (showAll) {
+            for (String tableName : allTables) {
+                displayedTests.add(tableName.substring(7));
+            }
+            if (displayedTests.size() == 0) {
+                error = NO_TESTS_ERROR;
+            }
+            header = ALL_HEADER;
+            buttonLabel = FAVORITES_BUTTON;
+            buttonIcon = UP_ARROW;
+            buttonLink = DASHBOARD_FAVORITES_LINK;
+        } else {
+            String email = currentUser.getEmail().trim().toLowerCase();
+            if (!StringUtils.isBlank(email)) {
+                Get get = new Get(Bytes.toBytes(currentUser.getEmail()));
+                get.addFamily(EMAIL_FAMILY);
+                Result result = table.get(get);
+                if (result != null) {
+                    List<Cell> cells = result.listCells();
+                    if (cells != null) {
+                        for (Cell cell : cells) {
+                            String test = Bytes.toString(cell.getQualifierArray());
+                            String val = Bytes.toString(cell.getValueArray());
+                            if (test != null && val.equals("1") && allTables.contains(test)) {
+                                displayedTests.add(test.substring(TABLE_PREFIX.length()));
+                            }
+                        }
+                    }
                 }
             }
-
-            String[] testArray = new String[testList.size()];
-            testList.toArray(testArray);
-            request.setAttribute("testNames", testArray);
-            dispatcher = request.getRequestDispatcher(DASHBOARD_MAIN_JSP);
-            try {
-                dispatcher.forward(request, response);
-            } catch (ServletException e) {
-                logger.info("Servlet Excpetion caught : ", e);
+            if (displayedTests.size() == 0) {
+                error = NO_FAVORITES_ERROR;
             }
-        } else {
-            response.sendRedirect(userService.createLoginURL(request.getRequestURI()));
+            header = FAVORITES_HEADER;
+            buttonLabel = ALL_BUTTON;
+            buttonIcon = DOWN_ARROW;
+            buttonLink = DASHBOARD_ALL_LINK;
+        }
+        Collections.sort(displayedTests);
+
+        String[] testArray = new String[displayedTests.size()];
+        displayedTests.toArray(testArray);
+        response.setContentType("text/plain");
+        request.setAttribute("testNames", testArray);
+        request.setAttribute("headerLabel", header);
+        request.setAttribute("showAll", showAll);
+        request.setAttribute("buttonLabel", buttonLabel);
+        request.setAttribute("buttonIcon", buttonIcon);
+        request.setAttribute("buttonLink", buttonLink);
+        request.setAttribute("logoutURL", logoutURI);
+        request.setAttribute("email", currentUser.getEmail());
+        request.setAttribute("error", error);
+        dispatcher = request.getRequestDispatcher(DASHBOARD_MAIN_JSP);
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            logger.info("Servlet Excpetion caught : ", e);
         }
     }
 }
---
+++ web/dashboard/appengine/servlet/src/main/java/com/google/android/vts/servlet/PreferencesServlet.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.google.android.vts.servlet;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Represents the servlet that is invoked on loading the preferences page to manage favorites.
+ */
+@WebServlet(name = "preferences", urlPatterns = {"/preferences"})
+public class PreferencesServlet extends HttpServlet {
+
+    private static final String PREFERENCES_JSP = "/preferences.jsp";
+    private static final String DASHBOARD_MAIN_LINK = "/";
+    private static final byte[] EMAIL_FAMILY = Bytes.toBytes("email_to_test");
+    private static final byte[] TEST_FAMILY = Bytes.toBytes("test_to_email");
+    private static final String STATUS_TABLE = "vts_status_table";
+    private static final String TABLE_PREFIX = "result_";
+    private static final Logger logger = LoggerFactory.getLogger(PreferencesServlet.class);
+
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Get the user's information
+        UserService userService = UserServiceFactory.getUserService();
+        User currentUser = userService.getCurrentUser();
+        RequestDispatcher dispatcher = null;
+
+        // If the user is logged out, allow them to log back in and return to the page.
+        // Set the logout URL to direct back to a login page that directs to the current request.
+        String loginURI = userService.createLoginURL(request.getRequestURI());
+        String logoutURI = userService.createLogoutURL(loginURI);
+
+        Table table = BigtableHelper.getTable(TableName.valueOf(STATUS_TABLE));
+        List<String> subscribedTests = new ArrayList<>();
+
+        String email = currentUser.getEmail().trim().toLowerCase();
+        if (!StringUtils.isBlank(email)) {
+            /* Get all cells for the row key = user email address within the column family EMAIL_FAMILY.
+             * Data is stored in the column qualifiers (test names) to represent a sparse
+             * adjacency matrix.
+             */
+            Get get = new Get(Bytes.toBytes(currentUser.getEmail()));
+            get.addFamily(EMAIL_FAMILY);
+            Result result = table.get(get);
+            if (result != null) {
+                List<Cell> cells = result.listCells();
+                if (cells != null) {
+                    for (Cell cell : cells) {
+                        String test = Bytes.toString(cell.getQualifierArray());
+                        String val = Bytes.toString(cell.getValueArray());
+                        if (test != null && test.startsWith(TABLE_PREFIX) && val.equals("1")) {
+                            subscribedTests.add(test.substring(TABLE_PREFIX.length()));
+                        }
+                    }
+                }
+            }
+        }
+
+        HTableDescriptor[] tables = BigtableHelper.getTables();
+        List<String> allTests = new ArrayList<>();
+
+        for (HTableDescriptor descriptor : tables) {
+            String tableName = descriptor.getNameAsString();
+            if (tableName.startsWith(TABLE_PREFIX)) {
+                allTests.add(tableName.substring(TABLE_PREFIX.length()));
+            }
+        }
+
+        response.setContentType("text/plain");
+        request.setAttribute("allTestsJson", new Gson().toJson(allTests));
+        request.setAttribute("subscribedTests", subscribedTests);
+        request.setAttribute("subscribedTestsJson", new Gson().toJson(subscribedTests));
+        request.setAttribute("logoutURL", logoutURI);
+        request.setAttribute("email", currentUser.getEmail());
+        dispatcher = request.getRequestDispatcher(PREFERENCES_JSP);
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            logger.info("Servlet Excpetion caught : ", e);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Get the current user's information.
+        UserService userService = UserServiceFactory.getUserService();
+        User currentUser = userService.getCurrentUser();
+        String email = currentUser.getEmail().trim().toLowerCase();
+
+        // Retrieve the added tests from the request.
+        String addedTestsString = request.getParameter("addedTests");
+        String[] addedTests = new String[0];
+        if (!StringUtils.isBlank(addedTestsString)) {
+            addedTests = addedTestsString.trim().split(",");
+        }
+
+        // Retrieve the removed tests from the request.
+        String removedTestsString = request.getParameter("removedTests");
+        String[] removedTests = new String[0];
+        if (!StringUtils.isBlank(removedTestsString)) {
+            removedTests = removedTestsString.trim().split(",");
+        }
+
+        // Get all of the table containing test data.
+        HTableDescriptor[] tables = BigtableHelper.getTables();
+        Set<String> allTables = new HashSet<>();
+        for (HTableDescriptor descriptor : tables) {
+            String tableName = descriptor.getNameAsString();
+            if (tableName.startsWith(TABLE_PREFIX)) {
+                allTables.add(tableName);
+            }
+        }
+
+        if (!StringUtils.isBlank(email)) {
+            Table table = BigtableHelper.getTable(TableName.valueOf(STATUS_TABLE));
+            // Add subscriptions
+            for (String test : addedTests) {
+                String tableName = TABLE_PREFIX + test;
+                if (StringUtils.isBlank(test) || !allTables.contains(tableName)) continue;
+                List<Put> puts = new ArrayList<>();
+
+                // Create the edge <test name> --> <email address> in the adjacency matrix
+                Put put = new Put(Bytes.toBytes(tableName));
+                put.addColumn(TEST_FAMILY, Bytes.toBytes(email), Bytes.toBytes("1"));
+                puts.add(put);
+
+                // Create the edge <email address> --> <test name> in the adjacency matrix
+                put = new Put(Bytes.toBytes(email));
+                put.addColumn(EMAIL_FAMILY, Bytes.toBytes(tableName), Bytes.toBytes("1"));
+                puts.add(put);
+                table.put(puts);
+            }
+
+            // Remove subscriptions
+            for (String test : removedTests) {
+                String tableName = TABLE_PREFIX + test;
+                if (StringUtils.isBlank(test)) continue;
+                List<Delete> deletes = new ArrayList<>();
+
+                // Delete the edge <test name> --> <email address> from the adjacency matrix
+                Delete del = new Delete(Bytes.toBytes(tableName));
+                del.addColumns(TEST_FAMILY, Bytes.toBytes(email));
+                deletes.add(del);
+
+                // Delete the edge <email address> --> <test name> from the adjacency matrix
+                del = new Delete(Bytes.toBytes(email));
+                del.addColumns(EMAIL_FAMILY, Bytes.toBytes(tableName));
+                deletes.add(del);
+                table.delete(deletes);
+            }
+        }
+        response.sendRedirect(DASHBOARD_MAIN_LINK);
+    }
+}
