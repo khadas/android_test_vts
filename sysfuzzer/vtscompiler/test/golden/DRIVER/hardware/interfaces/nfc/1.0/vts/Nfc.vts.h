6e8d5468f9f9cd2323d9b3c5c1f78ab215b4aaaf
Keun Soo Yim <yim@google.com>
2016-10-11 18:10:16 -0700

    extend vtsc regression test to driver mode
    Test: mma
    Change-Id: I2f2892d24a4fd43385b48ae0b8d5c86c107e8ec6

---
+++ sysfuzzer/vtscompiler/test/golden/DRIVER/hardware/interfaces/nfc/1.0/vts/Nfc.vts.h
@@ -0,0 +1,45 @@
+#ifndef __VTS_SPEC_Nfc.driver__
+#define __VTS_SPEC_Nfc.driver__
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#define LOG_TAG "FuzzerExtended_INfc"
+#include <utils/Log.h>
+#include <fuzz_tester/FuzzerBase.h>
+#include <fuzz_tester/FuzzerCallbackBase.h>
+#include <android/hardware/nfc/1.0/INfc.h>
+#include <android/hardware/nfc/1.0/types.h>
+#include <android/hardware/nfc/1.0/INfc.h>
+#include <hidl/HidlSupport.h>
+#include <hidl/IServiceManager.h>
+
+
+
+using namespace android::hardware;
+using namespace android::hardware::nfc::V1_0;
+namespace android {
+namespace vts {
+class FuzzerExtended_INfc : public FuzzerBase {
+ public:
+  FuzzerExtended_INfc() : FuzzerBase(HAL_HIDL), hw_binder_proxy_() { }
+ protected:
+  bool Fuzz(FunctionSpecificationMessage* func_msg,
+            void** result, const string& callback_socket_name);
+  bool GetAttribute(FunctionSpecificationMessage* func_msg,
+            void** result);
+  bool GetService();
+
+ private:
+  sp<INfc> hw_binder_proxy_;
+};
+
+extern "C" {
+extern android::vts::FuzzerBase* 
+vts_func_4_8_1_(
+);
+}
+}  // namespace vts
+}  // namespace android
+#endif
