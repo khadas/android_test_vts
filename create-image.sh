18e1ce47cdd696bfcc997dd5ade4ccdd8a7918be
Keun Soo Yim <yim@google.com>
2016-10-12 17:21:22 -0700

    update envsetup.sh
    Test: mma
    Change-Id: Ifc9aeef45b40d99f96b303dea7ec110d2c30846b

--- create-image.sh
+++ create-image.sh
@@ -3,7 +3,7 @@
 function vts_multidevice_create_image {
   DEVICE=$1
 
-  . ${ANDROID_BUILD_TOP}/build/envsetup.sh
+  . ${ANDROID_BUILD_TOP}/build/make/envsetup.sh
   cd ${ANDROID_BUILD_TOP}; lunch ${DEVICE}-userdebug
   cd ${ANDROID_BUILD_TOP}/test/vts; mma -j 32 && cd ${ANDROID_BUILD_TOP}; make vts adb -j 32
 }
