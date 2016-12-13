7291a3be62254a205b99afd0a4def2a136da1254
Keun Soo Yim <yim@google.com>
2016-12-12 16:21:09 -0800

    DO NOT MERGE - Uncomment targets non-builable on AOSP
    Test: mma
    Fixes: 33441867
    Change-Id: I9d2e3120f5b2263981c65be75c0e98ed4461ed8c
    (cherry picked from commit 354c3181948e562241758689401fcfe719a3ad35)

--- Android.mk
+++ Android.mk
@@ -42,27 +42,18 @@ $(VTS_PYTHON_ZIP): $(SOONG_ZIP)
 	$(hide) $(SOONG_ZIP) -d -o $@ -C test/vts-testcase -l $@.list
 	@rm -f $@.list
 	$(hide)unzip -o $@ -d $(VTS_TESTCASES_OUT)/vts/testcases/
-	@echo "build vts python package for audio effect HAL"
-	$(hide) find hardware/interfaces/audio/effect/2.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
 	# uncomment when audio effect HAL test has some py, config, or push files
+	#@echo "build vts python package for audio effect HAL"
+	# $(hide) find hardware/interfaces/audio/effect/2.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
 	# $(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/audio/effect/2.0/vts/functional -l $@.list
 	@rm -f $@.list
 	# $(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
-	@echo "build vts python package for NFC HAL"
-	$(hide) find hardware/interfaces/nfc/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
-	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/nfc/1.0/vts/functional -l $@.list
-	@rm -f $@.list
-	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
-	@echo "build vts python package for thermal HAL"
-	$(hide) find hardware/interfaces/thermal/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
-	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/thermal/1.0/vts/functional -l $@.list
-	@rm -f $@.list
-	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
-	@echo "build vts python package for vibrator HAL"
-	$(hide) find hardware/interfaces/vibrator/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
-	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/vibrator/1.0/vts/functional -l $@.list
-	@rm -f $@.list
-	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
+	# uncomment when NFC HAL test has some py, config, or push files
+	#@echo "build vts python package for NFC HAL"
+	#$(hide) find hardware/interfaces/nfc/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
+	#$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/nfc/1.0/vts/functional -l $@.list
+	#@rm -f $@.list
+	#$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
 	$(hide) touch -f $(VTS_TESTCASES_OUT)/vts/__init__.py
 
 $(VTS_CAMERAITS_ZIP): $(SOONG_ZIP)
