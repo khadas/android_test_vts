306058c50cc974627433b7d8a733f384357e7a77
Connor O'Brien <connoro@google.com>
2016-12-19 18:46:47 -0800

    Add code coverage for power HAL VTS tests
    Bug: 32022775
    Test: built and ran power tests
    Change-Id: I13689977df0f99705e50703526801350c271e522
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- Android.mk
+++ Android.mk
@@ -57,6 +57,12 @@ $(VTS_PYTHON_ZIP): $(SOONG_ZIP)
 	@rm -f $@.list
 	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
 	#
+	@echo "build vts python package for power HAL"
+	$(hide) find hardware/interfaces/power/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
+	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/power/1.0/vts/functional -l $@.list
+	@rm -f $@.list
+	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
+	#
 	@echo "build vts python package for thermal HAL"
 	$(hide) find hardware/interfaces/thermal/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
 	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/thermal/1.0/vts/functional -l $@.list
