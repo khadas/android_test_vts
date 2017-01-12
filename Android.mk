a5155a9451f26b4d1d6f10af6880ec34a409b497
Keun Soo Yim <yim@google.com>
2017-01-12 06:10:04 +0000

    Revert "Host-side change for soundtrigger HAL"
    This reverts commit bdaa8b2c5605674fda44b310463a5a69032a63b4.
    
    Change-Id: I39e213de0aa617622f9b67d168508c0f22736c2e

--- Android.mk
+++ Android.mk
@@ -118,12 +118,6 @@ $(VTS_PYTHON_ZIP): $(SOONG_ZIP)
 	@rm -f $@.list
 	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
 	#
-	@echo "build vts python package for soundtrigger HAL"
-	$(hide) find hardware/interfaces/soundtrigger/2.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
-	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/soundtrigger/2.0/vts/functional -l $@.list
-	@rm -f $@.list
-	$(hide) unzip $@ -d $(VTS_TESTCASES_OUT)
-	#
 	@echo "build vts python package for VR HAL"
 	$(hide) find hardware/interfaces/vr/1.0/vts/functional -name '*.py' -or -name '*.config' -or -name '*.push' | sort > $@.list
 	$(hide) $(SOONG_ZIP) -d -o $@ -C hardware/interfaces/vr/1.0/vts/functional -l $@.list
