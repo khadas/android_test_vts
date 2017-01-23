0d75c60caf7dac83d623e58209fee651eef5af3b
Nick Desaulniers <ndesaulniers@google.com>
2017-01-23 11:20:06 -0800

    Fingerprint VTS
    Change-Id: I6debe0876f20d7439797f9f5436e18127ff7a075

--- tools/build/tasks/list/vts_test_lib_hidl_package_list.mk
+++ tools/build/tasks/list/vts_test_lib_hidl_package_list.mk
@@ -101,6 +101,7 @@ vts_test_lib_hidl_packages += \
   bluetooth_hidl_hal_test \
   boot_hidl_hal_test \
   contexthub_hidl_hal_test \
+  fingerprint_hidl_hal_test \
   graphics_allocator_hidl_hal_test \
   graphics_mapper_hidl_hal_test \
   graphics_composer_hidl_hal_test \
