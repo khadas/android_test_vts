22e855f0bcfc86245e79b27c28947644d6136967
Zhuoyao Zhang <zhuoyao@google.com>
2017-03-24 13:52:57 -0700

    Update vts trace processor to support trace dedup
    * Rename VtsTraceParser to VtsTraceProcessor.
    
    Test: make vts.
    Change-Id: I87fb332e05f5a0a974dfbb1b3bdd4c7907346522

--- utils/native/trace_processor/VtsTraceProcessor.h
+++ utils/native/trace_processor/VtsTraceProcessor.h
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef TOOLS_TRACE_PROCESSOR_VTSTRACEPARSER_H_
-#define TOOLS_TRACE_PROCESSOR_VTSTRACEPARSER_H_
+#ifndef TOOLS_TRACE_PROCESSOR_VTSTRACEPROCESSOR_H_
+#define TOOLS_TRACE_PROCESSOR_VTSTRACEPROCESSOR_H_
 
 #include <android-base/macros.h>
 #include <test/vts/proto/VtsProfilingMessage.pb.h>
@@ -23,31 +23,34 @@
 namespace android {
 namespace vts {
 
-class VtsTraceParser {
+class VtsTraceProcessor {
  public:
-  VtsTraceParser() {};
-  virtual ~VtsTraceParser() {};
+  VtsTraceProcessor() {};
+  virtual ~VtsTraceProcessor() {};
 
   // Cleanups the given trace file to be used for replaying.
   // Current cleanup includes:
   // 1. remove duplicate trace item (e.g. passthrough event on the server side)
   // 2. remove trace item that could not be replayed (e.g. client event on the
   //    server side).
-  void CleanupTraceForReplay(std::string trace_file);
+  void CleanupTraceForReplay(const std::string& trace_file);
   // Parses the given trace file and outputs the latency for each API call.
-  void ProcessTraceForLatencyProfiling(std::string trace_file);
+  void ProcessTraceForLatencyProfiling(const std::string& trace_file);
+  // Parses all trace files under the the given trace directory and remove
+  // duplicate trace file.
+  void DedupTraces(const std::string& trace_dir);
 
  private:
   // Reads the trace file and parse each trace event into VtsProfilingRecord.
-  bool ParseTrace(std::string trace_file,
-      std::vector<VtsProfilingRecord>* records);
+  bool ParseTrace(const std::string& trace_file, bool ignore_timestamp,
+      bool entry_only, VtsProfilingMessage* profiling_msg);
   // Writes the given list of VtsProfilingRecord into an output file.
-  bool WriteRecords(std::string output_file,
+  bool WriteRecords(const std::string& output_file,
       const std::vector<VtsProfilingRecord>& records);
 
-  DISALLOW_COPY_AND_ASSIGN (VtsTraceParser);
+  DISALLOW_COPY_AND_ASSIGN (VtsTraceProcessor);
 };
 
 }  // namespace vts
 }  // namespace android
-#endif  // TOOLS_TRACE_PROCESSOR_VTSTRACEPARSER_H_
+#endif  // TOOLS_TRACE_PROCESSOR_VTSTRACEPROCESSOR_H_
