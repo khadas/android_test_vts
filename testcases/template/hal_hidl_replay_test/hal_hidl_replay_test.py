0ad26c115098fbf302e05357546612419e125ef8
Zhuoyao Zhang <zhuoyao@google.com>
2017-03-28 15:47:17 -0700

    Update hal hidl replay test template.
    * Inherit from binary_test.
    * Replay each trace file as a separate test case.
    
    Test: run vts -m VtsHalVibratorV1_0TargetReplay
    Change-Id: Iff70022c6d136c1d6381975bcf24aabf8d25060e
    (cherry picked from commit 4eba5194c171ec8d21a35ba9ba0b65177e96f5d7)

--- testcases/template/hal_hidl_replay_test/hal_hidl_replay_test.py
+++ testcases/template/hal_hidl_replay_test/hal_hidl_replay_test.py
@@ -17,62 +17,38 @@
 import logging
 import os
 
-from vts.runners.host import asserts
-from vts.runners.host import base_test
 from vts.runners.host import const
 from vts.runners.host import keys
 from vts.runners.host import test_runner
+from vts.testcases.template.binary_test import binary_test
+from vts.testcases.template.hal_hidl_replay_test import hal_hidl_replay_test_case
 from vts.utils.python.common import vintf_utils
 from vts.utils.python.controllers import android_device
 from vts.utils.python.os import path_utils
 
 
-class HalHidlReplayTest(base_test.BaseTestClass):
+class HalHidlReplayTest(binary_test.BinaryTest):
     """Base class to run a HAL HIDL replay test on a target device.
 
     Attributes:
         _dut: AndroidDevice, the device under test as config
-        shell: ShellMirrorObject, shell mirror
         DEVICE_TMP_DIR: string, target device's tmp directory path.
-        DEVICE_TRACE_FILE_PATH: string, the path of a trace file stored in
-                                a target device.
-        DEVICE_VTS_SPEC_FILE_PATH: string, the path of a VTS spec file
-                                   stored in a target device.
     """
 
     DEVICE_TMP_DIR = "/data/local/tmp"
-    DEVICE_TRACE_FILE_PATH = "/data/local/tmp/vts_replay_trace/trace_file.vts.trace"
-    DEVICE_VTS_SPEC_FILE_PATH = "/data/local/tmp/spec"
 
     def setUpClass(self):
         """Prepares class and initializes a target device."""
-        required_params = [
-            keys.ConfigKeys.IKEY_DATA_FILE_PATH,
-            keys.ConfigKeys.IKEY_HAL_HIDL_REPLAY_TEST_TRACE_PATHS,
-            keys.ConfigKeys.IKEY_HAL_HIDL_PACKAGE_NAME,
-            keys.ConfigKeys.IKEY_ABI_BITNESS
-        ]
-        opt_params = [keys.ConfigKeys.IKEY_BINARY_TEST_DISABLE_FRAMEWORK]
-        self.getUserParams(
-            req_param_names=required_params, opt_param_names=opt_params)
-
-        self.hal_hidl_package_name = str(self.hal_hidl_package_name)
-        self.abi_bitness = str(self.abi_bitness)
-
-        self._dut = self.registerController(android_device)[0]
-        self._dut.shell.InvokeTerminal("one")
-        self.shell = self._dut.shell.one
-
-        if self.coverage.enabled:
-            self.coverage.LoadArtifacts()
-            self.coverage.InitializeDeviceCoverage(self._dut)
-
-        self.shell.Execute("setenforce 0")  # SELinux permissive mode
-
-        # Stop Android runtime to reduce interference.
-        if getattr(self, keys.ConfigKeys.IKEY_BINARY_TEST_DISABLE_FRAMEWORK,
-                   False):
-            self._dut.stop()
+        super(HalHidlReplayTest, self).setUpClass()
+        # TODO(zhuoyao): consider to push trace just before each test case.
+        for trace_path in self.trace_paths:
+            trace_path = str(trace_path)
+            trace_file_name = str(os.path.basename(trace_path))
+            self._dut.adb.push(
+                path_utils.JoinTargetPath(self.data_file_path,
+                                          "hal-hidl-trace", trace_path),
+                path_utils.JoinTargetPath(self.DEVICE_TMP_DIR,
+                                          "vts_replay_trace", trace_file_name))
 
     def getServiceName(self):
         """Get service name(s) for the given hal."""
@@ -101,64 +77,62 @@ class HalHidlReplayTest(base_test.BaseTestClass):
                     service_names.add(hal_interface_instance)
         return service_names
 
-    def testReplay(self):
-        """Replays the given trace(s)."""
-        # TODO(yim): Use the InitHidlHal host-side stub and a stateful session
-        #            to control the VTS driver (replayer mode).
-        target_package, target_version = self.hal_hidl_package_name.split("@")
-        for trace_path in self.hal_hidl_replay_test_trace_paths:
-            trace_path = str(trace_path)
-            self._dut.adb.push(
-                os.path.join(self.data_file_path, "hal-hidl-trace",
-                             trace_path),
-                "/data/local/tmp/vts_replay_trace/trace_file.vts.trace")
+    # @Override
+    def CreateTestCases(self):
+        """Create a list of HalHidlReplayTestCase objects."""
+        required_params = [
+            keys.ConfigKeys.IKEY_HAL_HIDL_REPLAY_TEST_TRACE_PATHS,
+            keys.ConfigKeys.IKEY_HAL_HIDL_PACKAGE_NAME,
+            keys.ConfigKeys.IKEY_ABI_BITNESS
+        ]
+        opt_params = [keys.ConfigKeys.IKEY_BINARY_TEST_DISABLE_FRAMEWORK]
+        self.getUserParams(
+            req_param_names=required_params, opt_param_names=opt_params)
+        self.hal_hidl_package_name = str(self.hal_hidl_package_name)
+        self.abi_bitness = str(self.abi_bitness)
+        self.trace_paths = map(str, self.hal_hidl_replay_test_trace_paths)
 
-            custom_ld_library_path = path_utils.JoinTargetPath(
-                self.DEVICE_TMP_DIR, self.abi_bitness)
-            driver_binary_path = path_utils.JoinTargetPath(
-                self.DEVICE_TMP_DIR, self.abi_bitness,
-                "fuzzer%s" % self.abi_bitness)
-            target_vts_driver_file_path = path_utils.JoinTargetPath(
-                self.DEVICE_TMP_DIR, self.abi_bitness,
-                "%s@%s-vts.driver.so" % (target_package, target_version))
+        target_package, target_version = self.hal_hidl_package_name.split("@")
+        custom_ld_library_path = path_utils.JoinTargetPath(self.DEVICE_TMP_DIR,
+                                                           self.abi_bitness)
+        driver_binary_path = path_utils.JoinTargetPath(
+            self.DEVICE_TMP_DIR, self.abi_bitness,
+            "fuzzer%s" % self.abi_bitness)
+        target_vts_driver_file_path = path_utils.JoinTargetPath(
+            self.DEVICE_TMP_DIR, self.abi_bitness,
+            "%s@%s-vts.driver.so" % (target_package, target_version))
+
+        test_suite = ''
+        for trace_path in self.trace_paths:
+            logging.info("trace_path: %s", trace_path)
+            trace_file_name = str(os.path.basename(trace_path))
+            trace_path = path_utils.JoinTargetPath(
+                self.DEVICE_TMP_DIR, "vts_replay_trace", trace_file_name)
             service_names = self.getServiceName()
             for service_name in service_names:
-                cmd = ("LD_LIBRARY_PATH=%s:$LD_LIBRARY_PATH "
-                       "%s "
-                       "--mode=replay "
-                       "--trace_path=%s "
-                       "--spec_path=%s "
-                       "--hal_service_name=%s "
-                       "%s" % (custom_ld_library_path, driver_binary_path,
-                               self.DEVICE_TRACE_FILE_PATH,
-                               self.DEVICE_VTS_SPEC_FILE_PATH, service_name,
-                               target_vts_driver_file_path))
-
-                logging.info("Executing replay test command: %s", cmd)
-                command_results = self.shell.Execute(cmd)
-
-                asserts.assertTrue(command_results, "Empty command response.")
-                asserts.assertFalse(
-                    any(command_results[const.EXIT_CODE]),
-                    "Test failed with the following results: %s" %
-                    command_results)
+                test_name = "replay_test_" + trace_file_name + "_" + service_name
+                test_case = hal_hidl_replay_test_case.HalHidlReplayTestCase(
+                    trace_path,
+                    target_vts_driver_file_path,
+                    service_name,
+                    test_suite,
+                    test_name,
+                    driver_binary_path,
+                    ld_library_path=custom_ld_library_path)
+                self.testcases.append(test_case)
 
     def tearDownClass(self):
         """Performs clean-up tasks."""
-        # Restart Android runtime.
-        if getattr(self, keys.ConfigKeys.IKEY_BINARY_TEST_DISABLE_FRAMEWORK,
-                   False):
-            self._dut.start()
-
-        # Retrieve coverage if applicable
-        if self.coverage.enabled:
-            self.coverage.SetCoverageData(dut=self._dut, isGlobal=True)
-
         # Delete the pushed file.
-        cmd_results = self.shell.Execute("rm -f %s" %
-                                         self.DEVICE_TRACE_FILE_PATH)
-        if not cmd_results or any(cmd_results[const.EXIT_CODE]):
-            logging.warning("Failed to remove: %s", cmd_results)
+        for trace_path in self.trace_paths:
+            trace_file_name = str(os.path.basename(trace_path))
+            cmd_results = self.shell.Execute(
+                "rm -f %s" % path_utils.JoinTargetPath(
+                    self.DEVICE_TMP_DIR, "vts_replay_trace", trace_file_name))
+            if not cmd_results or any(cmd_results[const.EXIT_CODE]):
+                logging.warning("Failed to remove: %s", cmd_results)
+
+        super(HalHidlReplayTest, self).tearDownClass()
 
 
 if __name__ == "__main__":
