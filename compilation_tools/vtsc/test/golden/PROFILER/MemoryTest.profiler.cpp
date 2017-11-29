37d2f80938817bd2325deb2426c016e33339ba5c
Howard Chen <howardsoc@google.com>
2017-11-29 16:19:25 +0800

    Update the golden of IMemoryTest
    Regeneate the test files according to the latest IMemoryTest.
    
    Bug: 68295540
    Test: make vtsc_test
    Change-Id: I13a4b88cc5adb1a58d42a273b32f54d22814d697

--- compilation_tools/vtsc/test/golden/PROFILER/MemoryTest.profiler.cpp
+++ compilation_tools/vtsc/test/golden/PROFILER/MemoryTest.profiler.cpp
@@ -138,6 +138,146 @@ void HIDL_INSTRUMENTATION_FUNCTION_android_hardware_tests_memory_V1_0_IMemoryTes
         }
         profiler.AddTraceEvent(event, package, version, interface, msg);
     }
+    if (strcmp(method, "haveSomeMemoryBlock") == 0) {
+        FunctionSpecificationMessage msg;
+        msg.set_name("haveSomeMemoryBlock");
+        if (!args) {
+            LOG(WARNING) << "no argument passed";
+        } else {
+            switch (event) {
+                case details::HidlInstrumentor::CLIENT_API_ENTRY:
+                case details::HidlInstrumentor::SERVER_API_ENTRY:
+                case details::HidlInstrumentor::PASSTHROUGH_ENTRY:
+                {
+                    if ((*args).size() != 1) {
+                        LOG(ERROR) << "Number of arguments does not match. expect: 1, actual: " << (*args).size() << ", method name: haveSomeMemoryBlock, event type: " << event;
+                        break;
+                    }
+                    auto *arg_0 __attribute__((__unused__)) = msg.add_arg();
+                    ::android::hidl::memory::block::V1_0::MemoryBlock *arg_val_0 __attribute__((__unused__)) = reinterpret_cast<::android::hidl::memory::block::V1_0::MemoryBlock*> ((*args)[0]);
+                    if (arg_val_0 != nullptr) {
+                        arg_0->set_type(TYPE_STRUCT);
+                        profile____android__hidl__memory__block__V1_0__MemoryBlock(arg_0, (*arg_val_0));
+                    } else {
+                        LOG(WARNING) << "argument 0 is null.";
+                    }
+                    break;
+                }
+                case details::HidlInstrumentor::CLIENT_API_EXIT:
+                case details::HidlInstrumentor::SERVER_API_EXIT:
+                case details::HidlInstrumentor::PASSTHROUGH_EXIT:
+                {
+                    if ((*args).size() != 1) {
+                        LOG(ERROR) << "Number of return values does not match. expect: 1, actual: " << (*args).size() << ", method name: haveSomeMemoryBlock, event type: " << event;
+                        break;
+                    }
+                    auto *result_0 __attribute__((__unused__)) = msg.add_return_type_hidl();
+                    ::android::hidl::memory::block::V1_0::MemoryBlock *result_val_0 __attribute__((__unused__)) = reinterpret_cast<::android::hidl::memory::block::V1_0::MemoryBlock*> ((*args)[0]);
+                    if (result_val_0 != nullptr) {
+                        result_0->set_type(TYPE_STRUCT);
+                        profile____android__hidl__memory__block__V1_0__MemoryBlock(result_0, (*result_val_0));
+                    } else {
+                        LOG(WARNING) << "return value 0 is null.";
+                    }
+                    break;
+                }
+                default:
+                {
+                    LOG(WARNING) << "not supported. ";
+                    break;
+                }
+            }
+        }
+        profiler.AddTraceEvent(event, package, version, interface, msg);
+    }
+    if (strcmp(method, "set") == 0) {
+        FunctionSpecificationMessage msg;
+        msg.set_name("set");
+        if (!args) {
+            LOG(WARNING) << "no argument passed";
+        } else {
+            switch (event) {
+                case details::HidlInstrumentor::CLIENT_API_ENTRY:
+                case details::HidlInstrumentor::SERVER_API_ENTRY:
+                case details::HidlInstrumentor::PASSTHROUGH_ENTRY:
+                {
+                    if ((*args).size() != 1) {
+                        LOG(ERROR) << "Number of arguments does not match. expect: 1, actual: " << (*args).size() << ", method name: set, event type: " << event;
+                        break;
+                    }
+                    auto *arg_0 __attribute__((__unused__)) = msg.add_arg();
+                    ::android::hardware::hidl_memory *arg_val_0 __attribute__((__unused__)) = reinterpret_cast<::android::hardware::hidl_memory*> ((*args)[0]);
+                    if (arg_val_0 != nullptr) {
+                        arg_0->set_type(TYPE_HIDL_MEMORY);
+                        arg_0->mutable_hidl_memory_value()->set_size((*arg_val_0).size());
+                    } else {
+                        LOG(WARNING) << "argument 0 is null.";
+                    }
+                    break;
+                }
+                case details::HidlInstrumentor::CLIENT_API_EXIT:
+                case details::HidlInstrumentor::SERVER_API_EXIT:
+                case details::HidlInstrumentor::PASSTHROUGH_EXIT:
+                {
+                    if ((*args).size() != 0) {
+                        LOG(ERROR) << "Number of return values does not match. expect: 0, actual: " << (*args).size() << ", method name: set, event type: " << event;
+                        break;
+                    }
+                    break;
+                }
+                default:
+                {
+                    LOG(WARNING) << "not supported. ";
+                    break;
+                }
+            }
+        }
+        profiler.AddTraceEvent(event, package, version, interface, msg);
+    }
+    if (strcmp(method, "get") == 0) {
+        FunctionSpecificationMessage msg;
+        msg.set_name("get");
+        if (!args) {
+            LOG(WARNING) << "no argument passed";
+        } else {
+            switch (event) {
+                case details::HidlInstrumentor::CLIENT_API_ENTRY:
+                case details::HidlInstrumentor::SERVER_API_ENTRY:
+                case details::HidlInstrumentor::PASSTHROUGH_ENTRY:
+                {
+                    if ((*args).size() != 0) {
+                        LOG(ERROR) << "Number of arguments does not match. expect: 0, actual: " << (*args).size() << ", method name: get, event type: " << event;
+                        break;
+                    }
+                    break;
+                }
+                case details::HidlInstrumentor::CLIENT_API_EXIT:
+                case details::HidlInstrumentor::SERVER_API_EXIT:
+                case details::HidlInstrumentor::PASSTHROUGH_EXIT:
+                {
+                    if ((*args).size() != 1) {
+                        LOG(ERROR) << "Number of return values does not match. expect: 1, actual: " << (*args).size() << ", method name: get, event type: " << event;
+                        break;
+                    }
+                    auto *result_0 __attribute__((__unused__)) = msg.add_return_type_hidl();
+                    sp<::android::hidl::memory::token::V1_0::IMemoryToken> *result_val_0 __attribute__((__unused__)) = reinterpret_cast<sp<::android::hidl::memory::token::V1_0::IMemoryToken>*> ((*args)[0]);
+                    if (result_val_0 != nullptr) {
+                        result_0->set_type(TYPE_HIDL_INTERFACE);
+                        result_0->set_predefined_type("::android::hidl::memory::token::V1_0::IMemoryToken");
+                    } else {
+                        LOG(WARNING) << "return value 0 is null.";
+                    }
+                    break;
+                }
+                default:
+                {
+                    LOG(WARNING) << "not supported. ";
+                    break;
+                }
+            }
+        }
+        profiler.AddTraceEvent(event, package, version, interface, msg);
+    }
 }
 
 }  // namespace vts
