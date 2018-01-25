bcf49ec4c5c22e62fb9c0197e00ceb055b3a6955
jdesprez <jdesprez@google.com>
2018-01-25 14:47:55 -0800

    Vts incorrectly modified the expected DynamicConfig name
    DynamicConfig.xml is a special key name that is being searched
    by the BUILD_COMPATIBILITY_SUITE, it should not be renamed.
    
    Test: build vts, run cts-on-gsi observed the config being found
    Bug: 72504147
    
    Merged-In: Iaf49680f91d9b3442f84288b372554f2272801be
    Change-Id: Iaf49680f91d9b3442f84288b372554f2272801be
    (cherry picked from commit b920ab5d32104d7048ad1a4b433cf1983e261f30)

--- tools/build/tasks/vts_package.mk
+++ tools/build/tasks/vts_package.mk
@@ -112,7 +112,7 @@ target_hostdriven_copy_pairs := \
 
 host_additional_deps_copy_pairs := \
   test/vts/tools/vts-tradefed/etc/vts-tradefed_win.bat:$(VTS_TOOLS_OUT)/vts-tradefed_win.bat \
-  test/vts/tools/vts-tradefed/CtsDynamicConfig.xml:$(VTS_TESTCASES_OUT)/cts.dynamic
+  test/vts/tools/vts-tradefed/DynamicConfig.xml:$(VTS_TESTCASES_OUT)/cts.dynamic
 
 # Packaging rule for host-side Python logic, configs, and data files
 
