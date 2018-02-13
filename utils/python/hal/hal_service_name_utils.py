a533a54ea10380163bfdb010945e2b71ac7374b6
Zhuoyao Zhang <zhuoyao@google.com>
2018-02-13 15:32:41 -0800

    Add support for different hal service combination mode.
    * Currently support three different mode:
      -FULL_PERMUTATION: the default mode whith returns the full permutation
      of all the registered service instances. E.g. Hal service s1 with
      instances (n1, n2) and s2 with instances (n3, n4), return combination
      (s1/n1, s2/n3), (s1/n1, s2/n4), (s1/n2, s2/n3), (s1/n2, s2/n4).
      -NAME_MATCH: return the registered service instances with the same service name.
      E.g. Hal service s1 with instances (n1, n2) and s2 with instances (n1, n2),
      Return combination (s1/n1, s2/n1), (s1/n2, s2/n2).
      -NO_COMBINATION: Do not return the service instance combinations.
      This is used in cases when the test logic specifically handles the testing
      instances. E.g. drm tests.
    
    Test: make vts
          vts-tradefed run vts -m VtsHalLightV2_0Target
          vts-tradefed run vts -m VtsHalDrmV1_0Target
    
    Change-Id: Ia45afce588329509aa4a4e51d8672d10b3a28f81

--- utils/python/hal/hal_service_name_utils.py
+++ utils/python/hal/hal_service_name_utils.py
@@ -15,12 +15,14 @@
 #
 
 import json
+import logging
 
 from vts.runners.host import asserts
 from vts.runners.host import const
 
 VTS_TESTABILITY_CHECKER_32 = "/data/local/tmp/vts_testability_checker32"
 VTS_TESTABILITY_CHECKER_64 = "/data/local/tmp/vts_testability_checker64"
+
 
 def GetHalServiceName(shell, hal, bitness="64", run_as_compliance_test=False):
     """Determine whether to run a VTS test against a HAL and get the service
@@ -46,8 +48,8 @@ def GetHalServiceName(shell, hal, bitness="64", run_as_compliance_test=False):
     cmd += " -b " + bitness + " " + hal
     cmd_results = shell.Execute(str(cmd))
     asserts.assertFalse(
-            any(cmd_results[const.EXIT_CODE]),
-            "Failed to run vts_testability_checker.")
+        any(cmd_results[const.EXIT_CODE]),
+        "Failed to run vts_testability_checker.")
     result = json.loads(cmd_results[const.STDOUT][0])
     if str(result['Testable']).lower() == "true":
         return True, set(result['instances'])
@@ -55,8 +57,45 @@ def GetHalServiceName(shell, hal, bitness="64", run_as_compliance_test=False):
         return False, ()
 
 
-def GetServiceInstancesCombinations(services, service_instances):
+class CombMode(object):
+    """Enum for service name combination mode"""
+    FULL_PERMUTATION = 0
+    NAME_MATCH = 1
+    NO_COMBINATION = 2
+
+
+def GetServiceInstancesCombinations(services,
+                                    service_instances,
+                                    mode=CombMode.FULL_PERMUTATION):
+    """Create combinations of instances for all services.
+
+    Args:
+        services: list, all services used in the test. e.g. [s1, s2]
+        service_instances: dictionary, mapping of each service and the
+                           corresponding service name(s).
+                           e.g. {"s1": ["n1"], "s2": ["n2", "n3"]}
+        mode: CombMode that determines the combination strategy.
+
+    Returns:
+        A list of service instance combinations.
+    """
+    if mode == CombMode.FULL_PERMUTATION:
+        return GetServiceInstancesFullCombinations(services, service_instances)
+    elif mode == CombMode.NAME_MATCH:
+        return GetServiceInstancesNameMatchCombinations(services,
+                                                        service_instances)
+    else:
+        logging.warning("Unknown comb mode, use default comb mode instead.")
+        return GetServiceInstancesFullCombinations(services, service_instances)
+
+
+def GetServiceInstancesFullCombinations(services, service_instances):
     """Create all combinations of instances for all services.
+
+    Create full permutation for registered service instances, e.g.
+    s1 have instances (n1, n2) and s2 have instances (n3, n4), return all
+    permutations of s1 and s2, i.e. (s1/n1, s2/n3), (s1/n1, s2/n4),
+    (s1/n2, s2/n3) and (s1/n2, s2/n4)
 
     Args:
         services: list, all services used in the test. e.g. [s1, s2]
@@ -85,5 +124,44 @@ def GetServiceInstancesCombinations(services, service_instances):
                 new_instance_comb = [service + '/' + name]
                 new_instance_comb.extend(instance_comb)
                 service_instance_combinations.append(new_instance_comb)
+
+    return service_instance_combinations
+
+
+def GetServiceInstancesNameMatchCombinations(services, service_instances):
+    """Create service instance combinations with same name for services.
+
+    Create combinations for services with the same instance name, e.g.
+    both s1 and s2 have two instances with name n1, and n2, return
+    the service instance combination of s1 and s2 with same instance name,
+    i.e. (s1/n1, s2/n1) and (s1/n2, s2/n2)
+
+    Args:
+        services: list, all services used in the test. e.g. [s1, s2]
+        service_instances: dictionary, mapping of each service and the
+                           corresponding service name(s).
+                           e.g. {"s1": ["n1", "n2"], "s2": ["n1", "n2"]}
+
+    Returns:
+        A list of service instance combinations.
+        e.g. [[s1/n1, s2/n1], [s1/n2, s2/n2]]
+    """
+    service_instance_combinations = []
+    instance_names = set()
+    for service in services:
+        if service not in service_instances or not service_instances[service]:
+            logging.error("Does not found instance for service: %s", service)
+            return []
+        if not instance_names:
+            instance_names = set(service_instances[service])
+        else:
+            instance_names.intersection_update(set(service_instances[service]))
+
+    for name in instance_names:
+        instance_comb = []
+        for service in services:
+            new_instance = [service + '/' + name]
+            instance_comb.extend(new_instance)
+        service_instance_combinations.append(instance_comb)
 
     return service_instance_combinations
