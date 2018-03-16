f90aad6f10bd0d0e47507b69cd0c15db22e8ef8e
Hridya Valsaraju <hridya@google.com>
2018-03-15 17:05:16 -0700

    Move VtsDtboVerificationTest from staging to vts-firmware.xml
    Also renames the test to VtsFirmwareDtboVerification.
    
    Bug: 67779848
    Test: go/vts-review-bucket/aosp-master/CL_641760
    
    Change-Id: I22c0ed406ac7ae63a7af0ff859434aa3c5680316
    Merged-In: I22c0ed406ac7ae63a7af0ff859434aa3c5680316
    
    (cherry-picked from commit 3f9f9ad9b37a12650d5d21c6d521478823960b0c)

--- testcases/host/verify_dtbo/Android.mk
+++ testcases/host/verify_dtbo/Android.mk
@@ -16,6 +16,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := VtsDtboVerificationTest
+LOCAL_MODULE := VtsFirmwareDtboVerification
 VTS_CONFIG_SRC_DIR := testcases/host/verify_dtbo
 include test/vts/tools/build/Android.host_config.mk
