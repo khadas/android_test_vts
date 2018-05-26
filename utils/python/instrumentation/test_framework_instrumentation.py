73462fa50c18ae9401310c3d7a6172d25f5b8341
Yuexi Ma <yuexima@google.com>
2018-05-25 20:00:38 -0700

    simplify instrumentation api: support event.End()
    Previously, and event started in tfi should be ended by calling
      tfi.End(event)
    Now:
      event.End()
    
    * add public event.End() method
    * removed TestFrameworkInstrumentation class
    
    Test: python -m vts.utils.python.instrumentation.test_framework_instrumentation_test
    Bug: 80195850
    Change-Id: Ib39b46fca32b932fa990287cef443a731f93245c

--- utils/python/instrumentation/test_framework_instrumentation.py
+++ utils/python/instrumentation/test_framework_instrumentation.py
@@ -16,120 +16,64 @@
 
 import logging
 
-from vts.utils.python.instrumentation import test_framework_instrumentation_event as tfie
 from vts.utils.python.instrumentation import test_framework_instrumentation_categories as tfic
+from vts.utils.python.instrumentation import test_framework_instrumentation_event as tfie
 
 
-# global instance of TestFrameworkInstrumentation class
-global_instance = None
+# global category listing
 categories = tfic.TestFrameworkInstrumentationCategories()
 
 
-def GetInstance():
-    """Creates and gets the global instance of TestFrameworkInstrumentation class."""
-    global global_instance
-    if global_instance is None:
-        global_instance = TestFrameworkInstrumentation()
-
-    return global_instance
-
-
-def Begin(category, name=''):
+def Begin(category, name):
     """Marks the beginning of an event.
 
     Params:
         category: string, category of the event
-        name: string, name of the event, default is empty string.
+        name: string, name of the event
 
     Returns:
         Event object representing the event
     """
-    return GetInstance().Begin(category, name)
+    event = tfie.TestFrameworkInstrumentationEvent(category, name)
+    event.Begin()
+    return event
 
 
-def End(category_or_event, name=''):
+def End(category, name):
     """Marks the end of an event.
 
     When category string is provided, it will be matched to an event in
     internal event stack.
 
     Params:
-        category_or_event: string or Event, category of the event or event object
+        category: string, category of the event
+        name: string, name of the event
+
+    Returns:
+        Event object representing the event. None if cannot find an active matching event
+    """
+    event = FindEvent(category, name)
+    if not event:
+        logging.error('Event with category %s and name %s either does not '
+                      'exists or has already ended. Skipping...', category, name)
+        return None
+
+    event.End()
+    return event
+
+
+def FindEvent(category, name):
+    """Finds an existing event that has started given the names.
+
+    Params:
+        category: string, category of the event
         name: string, name of the event, default is empty string.
 
     Returns:
-        Event object representing the event
+        TestFrameworkInstrumentationEvent object if found; None otherwise.
     """
-    return GetInstance().End(category_or_event, name)
+    for event in reversed(tfie.event_stack):
+        if event.Match(category, name):
+            return event
 
-
-class TestFrameworkInstrumentation(object):
-    """A util class profile framework for performance analysis.
-
-    Attributes:
-        event_stack: list of Event objects, events that has began and not ended.
-    """
-    event_stack = []
-
-    def Begin(self, category, name=''):
-        """Marks the beginning of an event.
-
-        Params:
-            category: string, category of the event
-            name: string, name of the event, default is empty string.
-
-        Returns:
-            Event object representing the event
-        """
-        event = tfie.TestFrameworkInstrumentationEvent(category, name)
-        self.event_stack.append(event)
-        event.Begin()
-        return event
-
-    def End(self, category_or_event, name=''):
-        """Marks the end of an event.
-
-        When category string is provided, it will be matched to an event in
-        internal event stack.
-
-        Params:
-            category_or_event: string or Event, category of the event or event object
-            name: string, name of the event, default is empty string.
-
-        Returns:
-            Event object representing the event
-        """
-        if isinstance(category_or_event, tfie.TestFrameworkInstrumentationEvent):
-            if name:
-                logging.warn('TestFrameworkInstrumentation: Provided event with name. '
-                             'Ignoring the name.')
-            return self._EndEvent(category_or_event)
-
-        for event in reversed(self.event_stack):
-            if event.Match(category_or_event, name):
-                return self.End(event)
-
-        logging.error('TestFrameworkInstrumentation: Cannot find given category "%s" '
-                      'with name "%s".', category_or_event, name)
-
-        return None
-
-    def _EndEvent(self, event):
-        """Internal method to process an End Event.
-
-        Params:
-            event: Event object, the event
-        """
-        if event is None:
-            logging.warn('TestFrameworkInstrumentation: None event received and ignored.')
-            return
-
-        event.End()
-
-        if event in self.event_stack:
-            self.event_stack.remove(event)
-
-    def __del__(self):
-        if self.event_stack:
-            print('TestFrameworkInstrumentation: WARNING: event stack is not empty in the end: '
-                  '%s' % map(str, self.event_stack))
+    return None
