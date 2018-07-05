4edc848f97ee6e04b92be566a4bc4f93aa3d3bee
Hans Chen <junyec@google.com>
2018-07-04 17:19:55 -0700

    Added more tests for FMQ on host side
    Test: make vts -j; vts-tradefed run vts-host -m FmqTest
    Changes:
    * Mirrors FMQ test on the target side
    * Add a sanity check that it also works on other data types.
    
    Change-Id: I58c5c321f7cc2203de8d25346ca80a1b84ab1a07
    Merged-In: I58c5c321f7cc2203de8d25346ca80a1b84ab1a07
    (cherry picked from commit 8d68248ecff5c0499cdb00f5beb05756bfc32248)

--- testcases/codelab/fmq_test/VtsCodelabFmqTest.py
+++ testcases/codelab/fmq_test/VtsCodelabFmqTest.py
@@ -16,6 +16,8 @@
 #
 
 import logging
+import random
+import time
 
 from vts.runners.host import asserts
 from vts.runners.host import base_test
@@ -57,6 +59,50 @@ class VtsCodelabFmqTest(base_test.BaseTestClass):
         queue1_reader_id = self._queue1_reader.getQueueId()
         asserts.assertNotEqual(queue1_reader_id, -1)
 
+        # Initialize a blocking, synchronized writer.
+        self._queue2_writer = self.dut.resource.InitFmq(
+            data_type="uint32_t",
+            sync=True,
+            queue_size=2048,
+            blocking=True,
+            client=self.dut.hal.GetTcpClient("light"))
+        queue2_writer_id = self._queue2_writer.getQueueId()
+        asserts.assertNotEqual(queue2_writer_id, -1)
+
+        # Initialize a blocking, synchronized reader.
+        # This reader shares the same queue as self._queue2_writer.
+        self._queue2_reader = self.dut.resource.InitFmq(
+            existing_queue=self._queue2_writer,
+            client=self.dut.hal.GetTcpClient("light"))
+        queue2_reader_id = self._queue2_reader.getQueueId()
+        asserts.assertNotEqual(queue2_reader_id, -1)
+
+        # Initialize a non-blocking, unsynchronized writer.
+        self._queue3_writer = self.dut.resource.InitFmq(
+            data_type="double_t",
+            sync=False,
+            queue_size=2048,
+            blocking=False,
+            client=self.dut.hal.GetTcpClient("light"))
+        queue3_writer_id = self._queue3_writer.getQueueId()
+        asserts.assertNotEqual(queue3_writer_id, -1)
+
+        # Initialize a non-blocking, unsynchronized reader 1.
+        # This reader shares the same queue as self._queue3_writer.
+        self._queue3_reader1 = self.dut.resource.InitFmq(
+            existing_queue=self._queue3_writer,
+            client=self.dut.hal.GetTcpClient("light"))
+        queue3_reader1_id = self._queue3_reader1.getQueueId()
+        asserts.assertNotEqual(queue3_reader1_id, -1)
+
+        # Initialize a non-blocking, unsynchronized reader 2.
+        # This reader shares the same queue as self._queue3_writer and self._queue3_reader1.
+        self._queue3_reader2 = self.dut.resource.InitFmq(
+            existing_queue=self._queue3_writer,
+            client=self.dut.hal.GetTcpClient("light"))
+        queue3_reader2_id = self._queue3_reader2.getQueueId()
+        asserts.assertNotEqual(queue3_reader2_id, -1)
+
     def testBasic(self):
         """Tests correctness of basic util methods. """
         asserts.assertEqual(self._queue1_writer.getQuantumSize(), 2)
@@ -72,17 +118,145 @@ class VtsCodelabFmqTest(base_test.BaseTestClass):
         """This test operates on synchronized, non-blocking reader and writer.
            Tests a simple interaction between a writer and a reader.
         """
-        write_data = [1, 2, 3, 4, 5]
+        write_data = generateRandomIntegers(2048)
         read_data = []
         # Writer writes some data.
         asserts.assertTrue(
-            self._queue1_writer.write([1, 2, 3, 4, 5], 5),
-            "Write queue failed.")
+            self._queue1_writer.write(write_data, 2048), "Write queue failed.")
         # Check readers reads them back correctly.
-        read_success = self._queue1_reader.read(read_data, 5)
+        read_success = self._queue1_reader.read(read_data, 2048)
         asserts.assertTrue(read_success, "Read queue failed.")
-        asserts.assertEqual(read_data, [1, 2, 3, 4, 5])
+        asserts.assertEqual(read_data, write_data)
 
+    def testReadEmpty(self):
+        """Tests reading from an empty queue. """
+        read_data = []
+        read_success = self._queue1_reader.read(read_data, 5)
+        asserts.assertFalse(read_success,
+                            "Read should fail because queue is empty.")
+
+    def testWriteFull(self):
+        """Tests writes fail when queue is full. """
+        write_data = generateRandomIntegers(2048)
+        # This write should succeed.
+        asserts.assertTrue(
+            self._queue1_writer.write(write_data, 2048),
+            "Writer should write successfully.")
+        # This write should fail because queue is full.
+        asserts.assertFalse(
+            self._queue1_writer.write(write_data, 2048),
+            "Writer should fail because queue is full.")
+
+    def testWriteTooLarge(self):
+        """Tests writing more than queue capacity. """
+        write_data = generateRandomIntegers(2049)
+        asserts.assertFalse(
+            self._queue1_writer.write(write_data, 2049),
+            "Writer should fail since there are too many items to write.")
+
+    def testConsecutiveReadWrite(self):
+        """This test operates on synchronized, nonblocking reader and writer.
+           Tests consecutive interaction between reader and writer.
+        """
+        for i in range(64):
+            write_data = generateRandomIntegers(2048)
+            asserts.assertTrue(
+                self._queue1_writer.write(write_data, 2048),
+                "Writer should write successfully.")
+            read_data = []
+            read_success = self._queue1_reader.read(read_data, 2048)
+            asserts.assertTrue(read_success,
+                               "Reader should read successfully.")
+            asserts.assertEqual(write_data, read_data)
+
+        # Reader should have no more available to read.
+        asserts.assertEqual(0, self._queue1_reader.availableToRead())
+
+    def testBlockingReadWrite(self):
+        """This test operates on synchronized, blocking reader and writer.
+           Tests blocking read/write.
+           Writer waits 0.05s and writes.
+           Reader blocks for at most 0.1s, and should read successfully.
+           TODO: support this operation when reader and writer operates
+                 in parallel.
+        """
+        write_data = generateRandomIntegers(2048)
+        read_data = []
+
+        # Writer waits for 0.05s and writes.
+        time.sleep(0.05)
+        asserts.assertTrue(
+            self._queue2_writer.writeBlocking(write_data, 2048, 1000000),
+            "Writer should write successfully.")
+
+        # Reader reads.
+        read_success = self._queue2_reader.readBlocking(
+            read_data, 2048, 1000 * 1000000)
+        asserts.assertTrue(read_success,
+                           "Reader should read successfully after blocking.")
+        asserts.assertEqual(write_data, read_data)
+
+    def testBlockingTimeout(self):
+        """This test operates on synchronized, blocking reader.
+           Reader should time out because there is not data available.
+        """
+        read_data = []
+        read_success = self._queue2_reader.readBlocking(
+            read_data, 5, 100 * 1000000)
+        asserts.assertFalse(
+            read_success,
+            "Reader blocking should time out because there is no data to read."
+        )
+
+    def testUnsynchronizedReadWrite(self):
+        """This test operates on unsynchronized, nonblocking reader and writer.
+           Two readers can read back what writer writes.
+        """
+        write_data = generateRandomFloats(2048)
+        read_data1 = []
+        read_data2 = []
+        asserts.assertTrue(
+            self._queue3_writer.write(write_data, 2048),
+            "Writer should write successfully.")
+        read_success1 = self._queue3_reader1.read(read_data1, 2048)
+        read_success2 = self._queue3_reader2.read(read_data2, 2048)
+        asserts.assertTrue(read_success1, "Reader 1 should read successfully.")
+        asserts.assertTrue(read_success2, "Reader 2 should read successfully.")
+        asserts.assertEqual(write_data, read_data1)
+        asserts.assertEqual(write_data, read_data2)
+
+    def testIllegalBlocking(self):
+        """This test operates on unsynchronized, nonblocking writer.
+           Blocking is not allowed in unsynchronized queue.
+        """
+        write_data = generateRandomFloats(2048)
+        asserts.assertFalse(
+            self._queue3_writer.writeBlocking(write_data, 2048, 100 * 1000000),
+            "Blocking operation should fail in unsynchronized queue.")
+
+
+def generateRandomIntegers(data_size):
+    """Helper method to generate a list of  random integers between 0 and 100.
+
+    Args:
+        data_size: int, length of result list.
+
+    Returns:
+        int list, list of integers.
+    """
+    return [random.randint(0, 100) for i in range(data_size)]
+
+
+def generateRandomFloats(data_size):
+    """Helper method to generate a list of random floats between 0.0 and 100.0.
+
+    Args:
+        data_size: int, length of result list.
+
+    Returns:
+        float list, list of floats.
+    """
+    return [random.random() * 100 for i in range(data_size)]
 
 if __name__ == "__main__":
     test_runner.main()
