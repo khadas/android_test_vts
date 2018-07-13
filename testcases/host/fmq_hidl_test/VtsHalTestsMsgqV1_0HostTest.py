c00db37fe9f564ba01e8085d24ac7ba850ceeed0
Hans Chen <junyec@google.com>
2018-07-12 19:11:42 -0700

    Mirrors testcases for unsynchronized queue from FMQ target side test
    Test: make vts -j; vts-tradefed run vts-host -m VtsFmqHidlHostTest
    Changes:
    * Add tests for unsynchronized queue.
    
    Change-Id: Ica7508cf0cb0387c336c5d349ebba5df56dd4d83

--- testcases/host/fmq_hidl_test/VtsHalTestsMsgqV1_0HostTest.py
+++ testcases/host/fmq_hidl_test/VtsHalTestsMsgqV1_0HostTest.py
@@ -31,6 +31,9 @@ class VtsHalTestsMsgqV1_0HostTest(base_test.BaseTestClass):
        Since current host-side doesn't support long-form blocking or
        zero copy operations, all testcases related to these two features are not
        converted here.
+       UnsynchronizedWriteClientMultiProcess class is not converted here
+       because the testcase is similar to OverflowNotificationTest in
+       UnsynchronizedWriteClient class.
     """
     TEST_HAL_SERVICES = {"android.hardware.tests.msgq@1.0::ITestMsgQ"}
     MAX_NUM_MSG = 1024
@@ -102,6 +105,21 @@ class VtsHalTestsMsgqV1_0HostTest(base_test.BaseTestClass):
         asserts.assertTrue(
             sync_init_result,
             "Hal should configure a synchronized queue without error.")
+
+        # Initialize an unsynchronized queue on the server.
+        [success,
+         self._unsync_client1] = self._tests_msgq.getFmqUnsyncWrite(True)
+        asserts.assertTrue(
+            success,
+            "Hal should configure an unsynchronized queue without error.")
+        # An unsynchronized queue is registered successfully on the target driver.
+        asserts.assertNotEqual(self._unsync_client1, None)
+        asserts.assertNotEqual(self._unsync_client1.getQueueId(), -1)
+        # Register another reader.
+        self._unsync_client2 = self.dut.resource.InitFmq(
+            existing_queue=self._unsync_client1,
+            client=self.dut.hal.GetTcpClient("tests_msgq"))
+        asserts.assertNotEqual(self._unsync_client2.getQueueId(), -1)
 
     def testSyncQueueBlockingReadWriteSuccess(self):
         """This test operates on the synchronized queue.
@@ -332,6 +350,283 @@ class VtsHalTestsMsgqV1_0HostTest(base_test.BaseTestClass):
         asserts.assertTrue(
             self._tests_msgq.requestReadFmqSync(self.MAX_NUM_MSG),
             "Server should read successfully.")
+
+    def testUnsyncQueueSmallInputReaderTest1(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, SmallInputReaderTest1.
+           Server writes a small number of messages and client reads it back.
+        """
+        data_len = 16
+
+        # Server writes.
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(data_len),
+            "Server should write successfully.")
+        # Client reads.
+        read_data = []
+        asserts.assertTrue(
+            self._unsync_client1.read(read_data, data_len),
+            "Client should read successfully.")
+        asserts.assertEqual(read_data, generateSequentialData(16))
+
+    def testUnsyncQueueSmallInputWriterTest1(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, SmallInputWriterTest1.
+           Client writes a small number of messages and server reads it back.
+        """
+        data_len = 16
+        write_data = generateSequentialData(16)
+
+        # Client writes.
+        asserts.assertTrue(
+            self._unsync_client1.write(write_data, data_len),
+            "Server should write successfully.")
+        # Server reads.
+        asserts.assertTrue(
+            self._tests_msgq.requestReadFmqUnsync(data_len),
+            "Client should read successfully.")
+
+    def testUnsyncQueueReadWhenEmpty(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, ReadWhenEmpty.
+           Read should fail when queue is empty.
+        """
+        asserts.assertEqual(self._unsync_client1.availableToRead(), 0)
+        asserts.assertFalse(
+            self._unsync_client1.read([], 2),
+            "Client should fail to read because queue is empty.")
+
+    def testUnsyncQueueWriteWhenFull(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, WriteWhenFull.
+           Write should still succeed because unsynchronized queue
+           allows overflow. Subsequent read should fail.
+        """
+        write_data = generateSequentialData(self.MAX_NUM_MSG)
+
+        # Client writes.
+        asserts.assertTrue(
+            self._unsync_client1.write(write_data, self.MAX_NUM_MSG),
+            "Client should write successfully.")
+        asserts.assertEqual(self._unsync_client1.availableToWrite(), 0)
+        # Client tries to write more, still succeeds.
+        asserts.assertTrue(
+            self._unsync_client1.write([1], 1),
+            "Client should write successfully "
+            + "even if queue is full for unsynchronized queue.")
+        # Server should fail because queue overflows.
+        asserts.assertFalse(
+            self._tests_msgq.requestReadFmqUnsync(self.MAX_NUM_MSG),
+            "Server should fail to read because queue overflows.")
+
+    def testUnsyncQueueLargeInputTest1(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, LargeInputTest1.
+           Server writes to the queue and client reads the data back.
+        """
+        # Server writes.
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(self.MAX_NUM_MSG),
+            "Server should write successfully.")
+
+        write_data = generateSequentialData(self.MAX_NUM_MSG)
+        read_data = []
+        # Client reads.
+        asserts.assertEqual(self._unsync_client1.availableToRead(),
+                            self.MAX_NUM_MSG)
+        asserts.assertTrue(
+            self._unsync_client1.read(read_data, self.MAX_NUM_MSG),
+            "Client should read successfully.")
+        asserts.assertEqual(write_data, read_data)
+
+    def testUnsyncQueueLargeInputTest2(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, LargeInputTest2.
+           Server attempts to write more than the queue capacity and fails.
+        """
+        asserts.assertEqual(0, self._unsync_client1.availableToRead())
+        # Server attempts to write more than the queue capacity.
+        asserts.assertFalse(
+            self._tests_msgq.requestWriteFmqUnsync(self.MAX_NUM_MSG + 1),
+            "Server should fail because it writes more than queue capacity.")
+        # Check there is still no data for client.
+        asserts.assertEqual(0, self._unsync_client1.availableToRead())
+        asserts.assertFalse(
+            self._unsync_client1.read([], 1),
+            "Client should fail to read because queue is empty.")
+
+    def testUnsyncQueueLargeInputTest3(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, LargeInputTest3.
+           Client writes until the queue is full, and writes one more,
+           which overflows the queue. Read should fail after that.
+           Client writes again, and server should be able to read again.
+        """
+        write_data = generateSequentialData(self.MAX_NUM_MSG)
+
+        # Client fills up the queue.
+        asserts.assertTrue(
+            self._unsync_client1.write(write_data, self.MAX_NUM_MSG),
+            "Client should write successfully.")
+        asserts.assertEqual(0, self._unsync_client1.availableToWrite())
+        # Client attempts to write one more, still succeeds.
+        asserts.assertTrue(
+            self._unsync_client1.write([1], 1),
+            "Client should write successfully "
+            + "even if queue is full for unsynchronized queue.")
+        # Server fails to read because queue overflows.
+        asserts.assertFalse(
+            self._tests_msgq.requestReadFmqUnsync(self.MAX_NUM_MSG),
+            "Server should fail to read because queue overflows.")
+
+        # Do another interaction, and both should succeed.
+        asserts.assertTrue(
+            self._unsync_client1.write(write_data, self.MAX_NUM_MSG),
+            "Client should write successfully.")
+        asserts.assertTrue(
+            self._tests_msgq.requestReadFmqUnsync(self.MAX_NUM_MSG),
+            "Server should read successfully.")
+
+    def testUnsyncQueueClientMultipleRead(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, MultipleRead.
+           Server acts as a writer, and client reads the data back in batches.
+        """
+        chunk_size = 100
+        chunk_num = 5
+        num_messages = chunk_num * chunk_size
+        write_data = generateSequentialData(num_messages)
+
+        # Client has no data to read yet.
+        asserts.assertEqual(self._unsync_client1.availableToRead(), 0)
+        # Server writes.
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(num_messages),
+            "Server should write successfully.")
+
+        # Client reads it back continuously.
+        total_read_data = []
+        for i in range(chunk_num):
+            read_data = []
+            asserts.assertTrue(
+                self._unsync_client1.read(read_data, chunk_size),
+                "Client should read successfully.")
+            total_read_data.extend(read_data)
+
+        # Check read_data and write_data are equal.
+        asserts.assertEqual(write_data, total_read_data)
+
+    def testUnsyncQueueClientMultipleWrite(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, MultipleWrite.
+           Client writes the data in batches, and server reads it back together.
+        """
+        chunk_size = 100
+        chunk_num = 5
+        num_messages = chunk_num * chunk_size
+        write_data = generateSequentialData(num_messages)
+
+        # Client should see an empty queue.
+        asserts.assertEqual(self._unsync_client1.availableToWrite(),
+                            self.MAX_NUM_MSG)
+        for i in range(chunk_num):  # Client keeps writing.
+            curr_write_data = write_data[i * chunk_size:(i + 1) * chunk_size]
+            asserts.assertTrue(
+                self._unsync_client1.write(curr_write_data, chunk_size),
+                "Client should write successfully.")
+
+        # Server reads data back correctly.
+        asserts.assertTrue(
+            self._tests_msgq.requestReadFmqUnsync(num_messages),
+            "Server should read successfully.")
+
+    def testUnsyncQueueReadWriteWrapAround(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient, ReadWriteWrapAround.
+           Client writes half of the queue and server reads back.
+           Client writes the max capacity, which will cause a wrap
+           around in the queue, server should still read back correctly.
+        """
+        num_messages = self.MAX_NUM_MSG / 2
+        write_data = generateSequentialData(self.MAX_NUM_MSG)
+
+        # Client writes half of the queue capacity, and server reads it.
+        asserts.assertTrue(
+            self._unsync_client1.write(write_data, num_messages),
+            "Client should write successfully.")
+        asserts.assertTrue(
+            self._tests_msgq.requestReadFmqUnsync(num_messages),
+            "Server should read successfully.")
+        # Client writes the max queue capacity, causes a wrap around
+        asserts.assertTrue(
+            self._unsync_client1.write(write_data, self.MAX_NUM_MSG),
+            "Client should write successfully.")
+        # Server reads back data correctly
+        asserts.assertTrue(
+            self._tests_msgq.requestReadFmqUnsync(self.MAX_NUM_MSG),
+            "Server should read successfully.")
+
+    def testUnsyncQueueSmallInputMultipleReaderTest(self):
+        """This test operates on the unsynchronized queue.
+           Mirrors testcase: UnsynchronizedWriteClient,
+                             SmallInputMultipleReaderTest.
+           Server writes once, and two readers read the data back separately.
+        """
+        data_len = 16
+        # Server writes.
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(data_len),
+            "Server should write successfully.")
+        write_data = generateSequentialData(data_len)
+
+        # Client 1 reads back data correctly.
+        read_data1 = []
+        asserts.assertTrue(
+            self._unsync_client1.read(read_data1, data_len),
+            "Client 1 should read successfully.")
+        asserts.assertEqual(write_data, read_data1)
+        # Client 2 reads back data correctly.
+        read_data2 = []
+        asserts.assertTrue(
+            self._unsync_client2.read(read_data2, data_len),
+            "Client 2 should read successfully.")
+        asserts.assertEqual(write_data, read_data2)
+
+    def testUnsyncQueueOverflowNotificationTest(self):
+        """This test operates on the unsynchronized queue.
+           Mirror testcase: UnsynchronizedWriteClient, OverflowNotificationTest.
+           For unsynchronized queue, multiple readers can recover from
+           a write overflow condition.
+        """
+        # Server writes twice, overflows the queue.
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(self.MAX_NUM_MSG),
+            "Server should write successfully.")
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(self.MAX_NUM_MSG),
+            "Server should write successfully even if queue overflows.")
+
+        # Both clients fail to read.
+        read_data1 = []
+        read_data2 = []
+        asserts.assertFalse(
+            self._unsync_client1.read(read_data1, self.MAX_NUM_MSG),
+            "Client 1 should fail to read because queue overflows.")
+        asserts.assertFalse(
+            self._unsync_client2.read(read_data2, self.MAX_NUM_MSG),
+            "Client 2 should fail to read because queue overflows.")
+
+        # Server writes the data again.
+        asserts.assertTrue(
+            self._tests_msgq.requestWriteFmqUnsync(self.MAX_NUM_MSG),
+            "Server should write successfully.")
+        # Both clients should be able to read.
+        asserts.assertTrue(
+            self._unsync_client1.read(read_data1, self.MAX_NUM_MSG),
+            "Client 1 should read successfully.")
+        asserts.assertTrue(
+            self._unsync_client2.read(read_data2, self.MAX_NUM_MSG),
+            "Client 2 should read successfully.")
 
 
 def generateSequentialData(data_size):
