c03f95c430f908c9be37c7a33346aca6eebb64f4
Zhuoyao Zhang <zhuoyao@google.com>
2018-10-05 10:05:47 -0700

    Do not return if profiler sanity check failed.
    * For extended interfaces, hidl will pass the super interface name for
      profiling, in that case, the interface name will not matche the one we
      expecet (e.g. audio@4.0::IStreamOut extends from audio@4.0::IStream).
    * In general Return will cause missing of trace record, instead, just log warning
      and continue the profiling.
    
    Test: make vts
          vts-tradefed run vts-hal-profiling -m VtsHalAudioV4_0Target and
          check the recorded traces.
    
    Change-Id: I483ee3178421066b6e391e6da99b86256e472eb5

--- compilation_tools/vtsc/test/golden/PROFILER/TestMsgQ.profiler.cpp
+++ compilation_tools/vtsc/test/golden/PROFILER/TestMsgQ.profiler.cpp
@@ -25,18 +25,15 @@ void HIDL_INSTRUMENTATION_FUNCTION_android_hardware_tests_msgq_V1_0_ITestMsgQ(
         std::vector<void *> *args __attribute__((__unused__))) {
     if (strcmp(package, "android.hardware.tests.msgq") != 0) {
         LOG(WARNING) << "incorrect package. Expect: android.hardware.tests.msgq actual: " << package;
-        return;
     }
     std::string version_str = std::string(version);
     int major_version = stoi(version_str.substr(0, version_str.find('.')));
     int minor_version = stoi(version_str.substr(version_str.find('.') + 1));
     if (major_version != 1 || minor_version > 0) {
         LOG(WARNING) << "incorrect version. Expect: 1.0 or lower (if version != x.0), actual: " << version;
-        return;
     }
     if (strcmp(interface, "ITestMsgQ") != 0) {
         LOG(WARNING) << "incorrect interface. Expect: ITestMsgQ actual: " << interface;
-        return;
     }
 
     VtsProfilingInterface& profiler = VtsProfilingInterface::getInstance(TRACEFILEPREFIX);
