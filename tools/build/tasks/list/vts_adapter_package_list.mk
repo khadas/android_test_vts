109a4887aee680afce54b02217bd4acd18df8d11
Harpreet "Eli" Sangha <eliptus@google.com>
2018-12-28 18:53:44 +0900

    vibrator: Add Vibrator HAL 1.3
    Bug: 117835666
    Test: vts-tradefed run commandAndExit vts-hal -m VtsHalVibratorV1_3Target
    Change-Id: Id1e87f5e5a4bc971270de180684f08e08022ecb6
    Signed-off-by: Harpreet "Eli" Sangha <eliptus@google.com>

--- tools/build/tasks/list/vts_adapter_package_list.mk
+++ tools/build/tasks/list/vts_adapter_package_list.mk
@@ -216,6 +216,8 @@ vts_adapter_package_list := \
   android.hardware.vibrator@1.1-adapter-helper \
   android.hardware.vibrator@1.2-adapter \
   android.hardware.vibrator@1.2-adapter-helper \
+  android.hardware.vibrator@1.3-adapter \
+  android.hardware.vibrator@1.3-adapter-helper \
   android.hardware.vr@1.0-adapter \
   android.hardware.vr@1.0-adapter-helper \
   android.hardware.weaver@1.0-adapter \
