138984fff5013254823dc181374dea0d92160c20
lesl <lesl@google.com>
2019-01-17 11:20:32 +0800

    hostapd: add vts testcase for vts-tradefed
    Bug: 113799562
    Test: build - make vts
    Test: atest VtsHalWifiHostapdV1_1Target / atest VtsHalWifiHostapdV1_0Target
    Test: vts-tradefed run commandAndExit vts-hal --skip-all-system-status-check \
          --primary-abi-only --skip-preconditions --module \
          VtsHalWifiHostapdV1_0Target -l INFO
          vts-tradefed run commandAndExit vts-hal --skip-all-system-status-check \
          --primary-abi-only --skip-preconditions --module \
          VtsHalWifiHostapdV1_1Target -l INFO
    
    Change-Id: I3e0288c0144dc074798f9d01bc01a4c221295313

--- tools/build/tasks/list/vts_test_lib_hidl_package_list.mk
+++ tools/build/tasks/list/vts_test_lib_hidl_package_list.mk
@@ -302,6 +302,8 @@ vts_hal_test_bins := \
   VtsHalVibratorV1_2TargetTest \
   VtsHalVrV1_0TargetTest \
   VtsHalWeaverV1_0TargetTest \
+  VtsHalWifiHostapdV1_0TargetTest \
+  VtsHalWifiHostapdV1_1TargetTest \
   VtsHalWifiV1_0TargetTest \
   VtsHalWifiV1_1TargetTest \
   VtsHalWifiV1_2TargetTest \
