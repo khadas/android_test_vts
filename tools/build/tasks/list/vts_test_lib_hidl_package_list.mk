0f9e877d1785c212716e8a5799ecd9894e4a866e
Jeff Tinker <jtinker@google.com>
2019-05-07 16:42:34 -0700

    Add drm@1.2 vts tests
    Test for "add key status usable_in_future"
    
    Test: vts-tradefed run commandAndExit -m VtsHalDrmV1_2Target
    Bug:116738851
    Change-Id: I8cb9cf3ca014603f22f81017999ca85bd7043f64
    (cherry picked from commit 0093612565055754069e2bf0fefb310127f0cc0b)

--- tools/build/tasks/list/vts_test_lib_hidl_package_list.mk
+++ tools/build/tasks/list/vts_test_lib_hidl_package_list.mk
@@ -56,6 +56,7 @@ vts_hal_driver_libs := \
   android.hardware.contexthub@1.0-vts.driver \
   android.hardware.drm@1.0-vts.driver \
   android.hardware.drm@1.1-vts.driver \
+  android.hardware.drm@1.2-vts.driver \
   android.hardware.dumpstate@1.0-vts.driver \
   android.hardware.gatekeeper@1.0-vts.driver \
   android.hardware.gnss@1.0-vts.driver \
@@ -292,6 +293,7 @@ vts_hal_test_bins := \
   VtsHalContexthubV1_0TargetTest \
   VtsHalDrmV1_0TargetTest \
   VtsHalDrmV1_1TargetTest \
+  VtsHalDrmV1_2TargetTest \
   VtsHalDumpstateV1_0TargetTest \
   VtsHalEvsV1_0TargetTest \
   VtsHalGatekeeperV1_0TargetTest \
