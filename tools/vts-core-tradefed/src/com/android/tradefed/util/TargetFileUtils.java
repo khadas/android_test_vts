363d26de29cfe49b6dbee72bb712b0ecb0ddfa25
Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
2020-03-10 08:19:01 +0000

    Adding KernelSelinuxFileApiTest
    Bug: 147788056
    Test: atest KernelSelinuxFileApiTest
    
    Change-Id: I742eb1b8c80ec41fbd3d05a5c823b01d13ae8687
    Merged-In: I742eb1b8c80ec41fbd3d05a5c823b01d13ae8687

--- tools/vts-core-tradefed/src/com/android/tradefed/util/TargetFileUtils.java
+++ tools/vts-core-tradefed/src/com/android/tradefed/util/TargetFileUtils.java
@@ -17,6 +17,7 @@
 /* Utils for target file.*/
 package com.android.tradefed.util;
 
+import com.android.ddmlib.Log;
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.log.LogUtil.CLog;
@@ -25,6 +26,108 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TargetFileUtils {
+    // 3 permission groups: owner, group, all users
+    private static final int PERMISSION_GROUPS = 3;
+
+    public enum FilePermission {
+        EXECUTE(1),
+        READ(4),
+        WRITE(2);
+
+        private int mPermissionNum;
+
+        FilePermission(int permissionNum) {
+            mPermissionNum = permissionNum;
+        }
+
+        public int getPermissionNum() {
+            return mPermissionNum;
+        }
+    }
+
+    /**
+     * Determines if the permission bits grant the specify permission to any group.
+     *
+     * @param permission The specify permissions.
+     * @param permissionBits The octal permissions string (e.g. 741).
+     * @return True if any group has the specify permission.
+     */
+    public static boolean hasPermission(FilePermission permission, String permissionBits) {
+        for (int i = 0; i < PERMISSION_GROUPS; i++) {
+            if (hasPermission(permissionBits, i, permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Read the file permission bits of a path.
+     *
+     * @param filepath Path to a file or directory.
+     * @param device The test device.
+     * @return  Octal permission bits for the path.
+     */
+    public static String getPermission(String filepath, ITestDevice device)
+            throws DeviceNotAvailableException {
+        CommandResult commandResult = device.executeShellV2Command("stat -c %a " + filepath);
+        if (!CommandStatus.SUCCESS.equals(commandResult.getStatus())) {
+            CLog.logAndDisplay(Log.LogLevel.ERROR, "Get permission error:\nstdout%s\nstderr",
+                    commandResult.getStdout(), commandResult.getStderr());
+            return "";
+        }
+        return commandResult.getStdout().trim();
+    }
+
+    /**
+     * Determines if the permission bits grant a permission to a group.
+     *
+     * @param permissionBits The octal permissions string (e.g. 741).
+     * @param groupIndex The index of the group into the permissions string.
+     *                         (e.g. 0 is owner group). If set to -1, then all groups are
+     *                         checked.
+     * @param permission The value of the permission.
+     * @return  True if the group(s) has read permission.
+     */
+    private static boolean hasPermission(
+            String permissionBits, int groupIndex, FilePermission permission) {
+        if (groupIndex >= PERMISSION_GROUPS) {
+            throw new RuntimeException(String.format("Invalid group: %s", groupIndex));
+        }
+        if (permissionBits.length() != PERMISSION_GROUPS) {
+            throw new RuntimeException(
+                    String.format("Invalid permission bits: %s", permissionBits.length() + ""));
+        }
+
+        // Define the start/end group index
+        int start = groupIndex;
+        int end = groupIndex + 1;
+        if (groupIndex < 0) {
+            start = 0;
+            end = PERMISSION_GROUPS;
+        }
+
+        for (int i = start; i < end; i++) {
+            try {
+                int perm = Integer.valueOf(permissionBits.charAt(i) + "");
+                if (perm > 7) {
+                    throw new RuntimeException(String.format("Invalid permission bit: %d", perm));
+                }
+                if ((perm & permission.getPermissionNum()) == 0) {
+                    // Return false if any group lacks the permission
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                throw new RuntimeException(String.format(
+                        "Permission bits \"%s\" format error, should be three digital number "
+                                + "(e.q. 741).",
+                        permissionBits));
+            }
+        }
+        // Return true if no group lacks the permission
+        return true;
+    }
+
     /**
      * Helper method which executes a adb shell find command and returns the results as an {@link
      * ArrayList<String>}.
