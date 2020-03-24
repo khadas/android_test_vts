f27f9e8bd2b72098f986f67fd71c7cd5ab31ab66
Dan Shi <dshi@google.com>
2020-03-24 03:57:53 +0000

    Add dep for phony target vts10 am: 53125a9a72 am: 0d538732bc am: 7ccc12fedf am: f1e18415dc am: ab828275e0
    Bug: 151896491
    
    Change-Id: I1b9a1d2065f2566efa7e09123fc091884ece9905
    (cherry picked from commit 1c89ec9a8382430a88e13b7d4603608451555f7d)

--- tools/build/tasks/vts_package.mk
+++ tools/build/tasks/vts_package.mk
@@ -50,12 +50,14 @@ test_suite_tools += $(HOST_OUT_JAVA_LIBRARIES)/vts-core-tradefed-harness.jar \
 
 include $(BUILD_SYSTEM)/tasks/tools/compatibility.mk
 
-# Add a phony target to preparer vts to be renamed to vts10
-.PHONY: vts10
-
 .PHONY: vts
 vts: $(compatibility_zip) vtslab adb
-$(call dist-for-goals, vts, $(compatibility_zip))
+
+# Add a phony target to preparer vts to be renamed to vts10
+.PHONY: vts10
+vts10: vts
+
+$(call dist-for-goals, vts vts10, $(compatibility_zip))
 
 # Packaging rule for android-vts.zip's testcases dir (DATA subdir).
 target_native_modules := \
