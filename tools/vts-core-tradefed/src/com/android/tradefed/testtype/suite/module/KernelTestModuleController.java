daa27e2e65b92bc9f34dab0685eb94bf8d292d11
nelsonli <nelsonli@google.com>
2020-05-28 00:58:48 +0800

    [VTS] add option of is-hwasan and is-low-mem in KernelTestModuleController.
    Add option to check is-hwasan and is-low-mem in ltp config.
    
    Bug: 157435167
    Test: 1. m -j vts; (Run unit test)
            vts-tradefed run singleCommand host -n
            --console-result-reporter:suppress-passed-tests
            --class com.android.tradefed.testtype.suite.module.KernelTestModuleControllerTest
         2. patch ag/11606230
             atest vts_ltp_test_arm_64_hwasan
             atest vts_ltp_test_arm_64_lowmem
             atest vts_ltp_test_arm_64_lowmem_hwasan
             atest vts_ltp_test_arm_lowmem_hwasan
             atest vts_ltp_test_arm_hwasan
             atest vts_ltp_test_arm_lowmem
    Change-Id: I94e1be568574cb7e5500876697714fc3bb60db5a

--- tools/vts-core-tradefed/src/com/android/tradefed/testtype/suite/module/KernelTestModuleController.java
+++ tools/vts-core-tradefed/src/com/android/tradefed/testtype/suite/module/KernelTestModuleController.java
@@ -16,17 +16,33 @@
 package com.android.tradefed.testtype.suite.module;
 
 import com.android.tradefed.config.Option;
+import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.device.StubDevice;
 import com.android.tradefed.invoker.IInvocationContext;
 import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.util.AbiUtils;
 
 /** Base class for a module controller to not run tests when it doesn't match the architecture . */
 public class KernelTestModuleController extends BaseModuleController {
+    private final String lowMemProp = "ro.config.low_ram";
+    private final String productNameProp = "ro.product.name";
+
     @Option(name = "arch",
             description = "The architecture name that should run for this module."
                     + "This should be like arm64, arm, x86_64, x86, mips64, or mips.",
             mandatory = true)
     private String mArch = null;
+
+    @Option(name = "is-low-mem",
+            description = "If this option set to true, run this module if device prop"
+                    + "of 'ro.config.low_ram' is true else skip it.")
+    private boolean mIsLowMem = false;
+
+    @Option(name = "is-hwasan",
+            description = "If this option set to true, run this module if device prop "
+                    + "of 'ro.product.name' ended with _hwasan else skip it.")
+    private boolean mIsHwasan = false;
 
     @Override
     public RunStrategy shouldRun(IInvocationContext context) {
@@ -36,6 +52,34 @@ public class KernelTestModuleController extends BaseModuleController {
         // If moduleAbiName is arm64-v8a then the moduleArchName will be arm64
         // If moduleAbiName is armeabi-v7a then the moduleArchName will be arm
         String moduleArchName = AbiUtils.getArchForAbi(moduleAbiName);
+
+        if (mIsLowMem) {
+            if (!deviceLowMem(context)) {
+                CLog.d("Skipping module %s because %s is False.", getModuleName(), lowMemProp);
+                return RunStrategy.FULL_MODULE_BYPASS;
+            }
+        } else {
+            if (deviceLowMem(context)) {
+                CLog.d("Skipping module %s because the test is not for low memory device.",
+                        getModuleName());
+                return RunStrategy.FULL_MODULE_BYPASS;
+            }
+        }
+
+        if (mIsHwasan) {
+            if (!deviceWithHwasan(context)) {
+                CLog.d("Skipping module %s because %s is not ended with _hwasan.", getModuleName(),
+                        productNameProp);
+                return RunStrategy.FULL_MODULE_BYPASS;
+            }
+        } else {
+            if (deviceWithHwasan(context)) {
+                CLog.d("Skipping module %s because the test is for device of hwasan.",
+                        getModuleName());
+                return RunStrategy.FULL_MODULE_BYPASS;
+            }
+        }
+
         if (mArch.equals(moduleArchName)) {
             return RunStrategy.RUN;
         }
@@ -43,5 +87,58 @@ public class KernelTestModuleController extends BaseModuleController {
                         + "of %s.",
                 getModuleName(), moduleAbiName, mArch);
         return RunStrategy.FULL_MODULE_BYPASS;
+    }
+
+    private boolean deviceLowMem(IInvocationContext context) {
+        for (ITestDevice device : context.getDevices()) {
+            if (device.getIDevice() instanceof StubDevice) {
+                continue;
+            }
+            try {
+                String lowMemString = device.getProperty(lowMemProp);
+                boolean isLowMem = false;
+                if (lowMemString != null) {
+                    isLowMem = Boolean.parseBoolean(lowMemString);
+                } else {
+                    CLog.d("Cannot get the prop of %s.", lowMemProp);
+                }
+                if (isLowMem) {
+                    continue;
+                }
+                return false;
+            } catch (DeviceNotAvailableException e) {
+                CLog.e("Couldn't check prop of %s on %s", lowMemProp, device.getSerialNumber());
+                CLog.e(e);
+                throw new RuntimeException(e);
+            }
+        }
+        return true;
+    }
+
+    private boolean deviceWithHwasan(IInvocationContext context) {
+        for (ITestDevice device : context.getDevices()) {
+            if (device.getIDevice() instanceof StubDevice) {
+                continue;
+            }
+            try {
+                String productName = device.getProperty(productNameProp);
+                boolean isHwasan = false;
+                if (productName != null) {
+                    isHwasan = productName.contains("_hwasan");
+                } else {
+                    CLog.d("Cannot get the prop of %s.", productNameProp);
+                }
+                if (isHwasan) {
+                    continue;
+                }
+                return false;
+            } catch (DeviceNotAvailableException e) {
+                CLog.e("Couldn't check prop of %s on %s", productNameProp,
+                        device.getSerialNumber());
+                CLog.e(e);
+                throw new RuntimeException(e);
+            }
+        }
+        return true;
     }
 }
